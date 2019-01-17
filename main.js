ebdc5eec43bddba3b0911270c387faebd78bf276
new boi <46520916+Ultrigeon@users.noreply.github.com>
2019-01-17 09:58:09 +0100

    anon has been murdered - attempting to reconstruct his soul
    i wish anon were not kill

---
+++ main.js
@@ -0,0 +1,33 @@
+/****************************
+ * in loving memory of anon *
+ ****************************/
+
+Discord = require("discord.js")
+anon2 = new Discord.Client()
+
+axios = require("axios")
+
+anon2.on("ready", on_ready)
+anon2.on("message", on_message)
+
+async function on_ready(){
+	console.log("bless anon's soul")
+}
+
+async function on_message( message ){
+	var input = message.content.toLowerCase()
+	var parameters = message.content.split(" ")
+	parameters.shift()
+
+	if( message.content == "!neko" ){
+		var meow = await axios.get("http://aws.random.cat/meow")
+		message.reply(meow.data.file)
+	}
+	
+	if( message.content == "!youtube" ){
+		message.reply("wip")
+	}
+	
+}
+
+anon.login(process.env.anon)
