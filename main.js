0ef25158da523003b16194db7d112c1ec9c5eaab
new boi <46520916+Ultrigeon@users.noreply.github.com>
2019-01-18 12:24:55 +0100

    fitted the names and reformated the file

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ axios = require("axios")
 
 Cleverbot = require('cleverbot.io')
 
-cleverbot_client = new Cleverbot(process.env.cleverbot_user, process.env.cleverbot_key)
-cleverbot_client.my_body_is_ready = false
-cleverbot_client.setNick('AnonsSpirit');
+cbot = new Cleverbot(process.env.cleverbot_user, process.env.cleverbot_key)
+cbot.my_body_is_ready = false
+cbot.setNick('AnonsSpirit');
 
 bot.on("ready", on_ready)
 bot.on("message", on_message)
@@ -19,7 +19,7 @@ bot.on("error", on_error)
 
 async function on_ready(){
 	console.log("bless anon's soul")
-	cleverbot_client.create((err, s)=>{ cleverbot_client.my_body_is_ready = true })
+	cbot.create((err, s)=>{ cbot.is_ready = true })
 	bot.admins = typeof process.env.admins != "undefined" ? process.env.admins.split(",") : []
 }
 
@@ -28,29 +28,35 @@ async function on_message( message ){
 	var parameters = message.content.split(" ")
 	parameters.shift()
 
+	//  neko command
 	if( message.content == ".neko" ){
 		var meow = await axios.get("http://aws.random.cat/meow")
 		message.reply(meow.data.file)
 	}
 	
+	//  youtube command
 	if( message.content == ".yt" ){
 		message.reply("wip")
 	}
 	
+	//  say and delete command
 	if( message.content.startsWith(".sayd ") ){
 		var parameter = message.content.substr(".sayd ".length)
 		message.channel.send(parameter)
 		message.delete()
 	}
 
+	//  eval block
 	if( bot.admins.includes(message.author.id) ){
 		require("./js/eval.js")(message)
 	}
 	
-	if( message.channel.topic.toLowerCase().includes("cleverbot") ){
-		if( cleverbot_client.my_body_is_ready && message.author.bot == false ){
-			cleverbot_client.ask(message.content, (err, r) => {
-				message.channel.send(r)
+	//  cleverbot responses at cleverbot channel
+	var is_cleverbot_channel = !!message.channel.topic && message.channel.topic.toLowerCase().includes("cleverbot")
+	if( is_cleverbot_channel ){
+		if( cbot.is_ready && !message.author.bot ){
+			cbot.ask(message.content, (err, response)=>{
+				message.channel.send(response)
 			})
 		}
 	}
