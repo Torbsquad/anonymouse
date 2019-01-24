88c060f3d82e54cc37e792ea643df4e3195f7a44
Ultrigeon <Ultrigeon@pm.me>
2019-01-24 19:39:47 +0100

    added error handlers

--- js/web.js
+++ js/web.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const PORT = process.env.PORT || 5000
+express = require('express')
+PORT = process.env.PORT || 5000
 express()
     .get( '/', (req, res) => res.send('<pre>o/</pre>') )
     .listen( PORT, () => console.log(`Listening on ${ PORT }`) )
