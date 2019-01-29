5a34e675daa55bea1ceea88635de5ad720361c74
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-01-29 08:41:13 +0100

    inserted the api-image sender

--- js/p.js
+++ js/p.js
@@ -2,5 +2,16 @@ module.exports = main
 
 async function main(bot){
 	var target_channel = bot.channels.find(c=>c.id=="539700451059171338")
-	console.log(target_channel)
+	
+	var axios = require("axios")
+
+	var e = await axios.get(target_channel.topic)
+	var b = e.data.items.filter(d=>d.user!="pr0gramm")
+
+	if(b[0].image.match(/.*?\.(gif|png|jpg)$/)){
+	    target_channel.send("https://img.pr0gramm.com/"+b[0].image)
+	}
+	if(b[0].image.match(/.*?\.(mp4)$/)){
+	    target_channel.send("https://vid.pr0gramm.com/"+b[0].image)
+	}
 }
