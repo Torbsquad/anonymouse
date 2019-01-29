1870d36337312938921615047acfe3cd2b01dd49
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-01-29 08:58:56 +0100

    added "already send"-checker

--- js/p.js
+++ js/p.js
@@ -8,10 +8,32 @@ async function main(bot){
 	var e = await axios.get(target_channel.topic)
 	var b = e.data.items.filter(d=>d.user!="pr0gramm")
 
-	if(b[0].image.match(/.*?\.(gif|png|jpg)$/)){
-	    target_channel.send("https://img.pr0gramm.com/"+b[0].image)
+	var img = {
+		suffix: b[0].image,
+		type: datatype(b[0].image)
 	}
-	if(b[0].image.match(/.*?\.(mp4)$/)){
-	    target_channel.send("https://vid.pr0gramm.com/"+b[0].image)
+	
+	if(img.type){
+		var link = `https://${img.type}.pr0gramm.com/${img.suffix}`
+		var already_there = await does_already_contain(link,target_channel)
+		if(!already_there){
+	    	target_channel.send(link)
+		}
 	}
+	
+}
+
+async function does_already_contain(message,channel){
+	var messages = await channel.fetchMessages({limit:100})
+	return messages.map(m=>m.content).some(m=>m==message)
+}
+
+function datatype(t){
+	if(t.image.match(/.*?\.(gif|png|jpg)$/)){
+		return "img"
+	}
+	if(t.image.match(/.*?\.(gif|png|jpg)$/)){
+		return "vid"
+	}
+	return false
 }
