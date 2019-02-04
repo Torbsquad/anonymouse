aa05634f3c952e7952b0fde77a7d1fe3783e7792
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-04 15:49:33 +0100

    added temperature command

--- main.js
+++ main.js
@@ -130,6 +130,20 @@ async function on_message( message ){
 			})
 		}
 	}
+	
+	if( input.command.ci == ".temp" ){
+		let city = input.parameters.raw
+		let apiKey = process.env.open_weather_map_token
+		let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+		
+		try{
+		    wetter = await axios.get(apiUrl)
+		    message.reply(`Es ist in ${city} ${wetter.data.main.temp}°C`)
+		}
+		catch(err){
+		    message.reply(err.message)
+		}
+	}
 }
 
 function _on_error(err){
