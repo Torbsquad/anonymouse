e2de01ed538a8138565636c0f87f53b31fc1e80c
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-04 18:06:51 +0100

    changed the formating of the .colors code

--- main.js
+++ main.js
@@ -87,17 +87,17 @@ async function on_message( message ){
 	}
 	if( [".colors",".colours"].includes(input.command.ci) ){
 		var response = `__ColorResolvable:__
-						Hexadezimalausdrücke als Zahl oder Wort
-						→ (\`#123ABC\`, \`0x123ABC\`, \`123ABC\`, ...)
+			Hexadezimalausdrücke als Zahl oder Wort
+			→ (\`#123ABC\`, \`0x123ABC\`, \`123ABC\`, ...)
 
-						eigentlich auch Ganzzahl selbst,.... eigentlich
+			eigentlich auch Ganzzahl selbst,.... eigentlich
 
-						RGB-Liste also [R,G,B]
-						→ (\`[255,0,255]\`, \`[123, 234, 45]\`)
+			RGB-Liste also [R,G,B]
+			→ (\`[255,0,255]\`, \`[123, 234, 45]\`)
 
-						Oder einer dieser Farben: *(in .color wird klein geschriebenes groß)*
-						→ \`DEFAULT\`, \`AQUA\`, \`GREEN\`, \`BLUE\`, \`PURPLE\`, \`LUMINOUS_VIVID_PINK\`, \`GOLD\`, \`ORANGE\`, \`RED\`, \`GREY\`, \`DARKER_GREY\`, \`NAVY\`, \`DARK_AQUA\`, \`DARK_GREEN\`, \`DARK_BLUE\`, \`DARK_PURPLE\`, \`DARK_VIVID_PINK\`, \`DARK_GOLD\`, \`DARK_ORANGE\`, \`DARK_RED\`, \`DARK_GREY\`, \`LIGHT_GREY\`, \`DARK_NAVY\`, \`RANDOM\``
-		message.channel.send(response.replace(/\n\t\t\t\t\t\t/g,"\n"))
+			Oder einer dieser Farben: *(in .color wird klein geschriebenes groß)*
+			→ \`DEFAULT\`, \`AQUA\`, \`GREEN\`, \`BLUE\`, \`PURPLE\`, \`LUMINOUS_VIVID_PINK\`, \`GOLD\`, \`ORANGE\`, \`RED\`, \`GREY\`, \`DARKER_GREY\`, \`NAVY\`, \`DARK_AQUA\`, \`DARK_GREEN\`, \`DARK_BLUE\`, \`DARK_PURPLE\`, \`DARK_VIVID_PINK\`, \`DARK_GOLD\`, \`DARK_ORANGE\`, \`DARK_RED\`, \`DARK_GREY\`, \`LIGHT_GREY\`, \`DARK_NAVY\`, \`RANDOM\``
+		message.channel.send(response.replace(/\n\t+/g,"\n"))
 		
 		message.channel.send("https://discord.js.org/#/docs/main/stable/typedef/ColorResolvable")
 	}
