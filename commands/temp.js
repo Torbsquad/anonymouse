7dde4527b4b77c6a92ef032a1257fa2c5441fb8b
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-09 00:16:21 +0100

    added temp command to the new system

---
+++ commands/temp.js
@@ -0,0 +1,15 @@
+const axios = require("axios")
+
+module.exports = async (bot, message, args) => {
+  let city = args
+  let apiKey = process.env.open_weather_map_token
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURI(city)}&units=metric&appid=${apiKey}`
+
+  try{
+      wetter = await axios.get(apiUrl)
+      message.reply(`Es ist in ${city} ${wetter.data.main.temp}°C`)
+  }
+  catch(err){
+      message.reply(err.message)
+  }
+}
