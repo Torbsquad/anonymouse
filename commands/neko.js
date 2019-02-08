6a124db59834f17d6eed76773c7f7ae8479c76e4
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-09 00:09:25 +0100

    made it an async function

--- commands/neko.js
+++ commands/neko.js
@@ -1,6 +1,6 @@
 const axios = require("axios")
 
-module.exports = (bot, message, args) => {
+module.exports = async (bot, message, args) => {
 		var meow = await axios.get("http://aws.random.cat/meow")
 		message.reply(meow.data.file)
 }
