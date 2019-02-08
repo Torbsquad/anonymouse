c25a21706615ae02ca170ac708360bb4dc9870d6
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-09 00:23:20 +0100

    added colors command

---
+++ commands/color.js
@@ -0,0 +1,11 @@
+module.exports = (bot, message, args) => {
+  var target_member = message.guild.members.find(member => member.id == message.author.id)
+  var colorrole = role => role.name == "Farbe" || role.name == target_member.name || role.name[0] == "ܿ"
+  var target_role = target_member.roles.find(colorrole)
+  if( !args.match(/^\[.{1,}?\,.{1,}?\,.{1,}?\]$/) ){
+    target_role.setColor(args.toUpperCase())
+  }
+  else{
+    target_role.setColor(JSON.parse(args))
+  }
+}
