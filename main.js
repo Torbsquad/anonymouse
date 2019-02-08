0f3ad12d9ec6e896a13cf013b7ae0efab5cd46f5
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-09 00:39:07 +0100

    const and spaces vs tabs

--- main.js
+++ main.js
@@ -2,15 +2,14 @@
  * in loving memory of anon *
  ****************************/
 
-Discord = require("discord.js")
-bot = new Discord.Client()
+const Discord = require("discord.js")
+const bot = new Discord.Client()
 
-axios = require("axios")
-sleep = require("./js/sleep")
+const sleep = require("./js/sleep")
 
-Cleverbot = require('cleverbot.io')
+const Cleverbot = require('cleverbot.io')
 
-fs = require("fs")
+const fs = require("fs")
 var commands = {}
 
 bot.on("ready",()=>{
@@ -51,23 +50,23 @@ async function on_ready(){
 }
 
 async function on_message( message ){
-	var input = {
-		command: {
-			// case-senstivie, unmodified
-			cs: message.content.split(" ")[0],
-			// case-insenstivie, pushed to lowercase
-			ci: message.content.toLowerCase().split(" ")[0]
-		},
-		parameters: {
-			// case-senstivie, unmodified
-			cs: message.content.split(" ").slice(1),
-			// case-insenstivie, pushed to lowercase
-			ci: message.content.toLowerCase().split(" ").slice(1),
-			// complete ((without)) split
-			raw: message.content.split(" ").slice(1).join(" ")
-		}
-	}
-	
+    var input = {
+        command: {
+            // case-senstivie, unmodified
+            cs: message.content.split(" ")[0],
+            // case-insenstivie, pushed to lowercase
+            ci: message.content.toLowerCase().split(" ")[0]
+        },
+        parameters: {
+            // case-senstivie, unmodified
+            cs: message.content.split(" ").slice(1),
+            // case-insenstivie, pushed to lowercase
+            ci: message.content.toLowerCase().split(" ").slice(1),
+            // complete ((without)) split
+            raw: message.content.split(" ").slice(1).join(" ")
+        }
+    }
+
     if( commands[input.command.ci] ){
         try{
             commands[input.command.ci](bot, message, input.parameters.raw)
@@ -77,47 +76,47 @@ async function on_message( message ){
         }
     }
 
-	//  eval block
-	if( bot.admins.includes(message.author.id) ){
-		require("./js/eval.js")(message)
-	}
-	
-	//  cleverbot responses at cleverbot channel
-	var is_cleverbot_channel = !!message.channel.topic && message.channel.topic.toLowerCase().includes("cleverbot")
-	if( cbot.is_ready && ( is_cleverbot_channel || message.isMentioned(bot.user) ) ){
-		var ignorelist = [/^t!/,/^$/,/^!/,/^\./,/^\/\//,/```js/]
-		var question = message.cleanContent
-		
-		var content_not_in_ignorelist = !ignorelist.some(e=>question.match(e))
-		question = question.split(bot.user.username).join(cbot.nick).replace(/@/g,"")
-		if( !message.author.bot && content_not_in_ignorelist ){
-			message.channel.startTyping()
-			cbot.ask(question, (err, response)=>{
-				response = response.replace(/\*/g,"\\*")
-				message.channel.send(response)
-				message.channel.stopTyping()
-			})
-		}
-	}
+    //  eval block
+    if( bot.admins.includes(message.author.id) ){
+        require("./js/eval.js")(message)
+    }
+    
+    //  cleverbot responses at cleverbot channel
+    var is_cleverbot_channel = !!message.channel.topic && message.channel.topic.toLowerCase().includes("cleverbot")
+    if( cbot.is_ready && ( is_cleverbot_channel || message.isMentioned(bot.user) ) ){    
+        var ignorelist = [/^t!/,/^$/,/^!/,/^\./,/^\/\//,/```js/]
+        var question = message.cleanContent
+
+        var content_not_in_ignorelist = !ignorelist.some(e=>question.match(e))
+        question = question.split(bot.user.username).join(cbot.nick).replace(/@/g,"")
+        if( !message.author.bot && content_not_in_ignorelist ){
+            message.channel.startTyping()
+            cbot.ask(question, (err, response)=>{
+                response = response.replace(/\*/g,"\\*")
+                message.channel.send(response)
+                message.channel.stopTyping()
+            })
+        }
+    }
 
 }
 
 async function on_error( err ){
-	console.log("on_error @ discord-anon",err)
+    console.log("on_error @ discord-anon",err)
 }
 
 // if no cleverbot.io token is given, should no cleverbot be prepared
 if( !!process.env.cleverbot_user && !!process.env.cleverbot_key ){
-	// loging in at cleverbot
-	cbot = new Cleverbot(process.env.cleverbot_user, process.env.cleverbot_key)
-	cbot.is_ready = false
-	cbot.is_active = true
-	cbot.setNick('AnonsSpirit');
+    // loging in at cleverbot
+    cbot = new Cleverbot(process.env.cleverbot_user, process.env.cleverbot_key)
+    cbot.is_ready = false
+    cbot.is_active = true
+    cbot.setNick('AnonsSpirit');
 }
 else{
-	cbot = {}
-	cbot.is_ready = false
-	cbot.is_active = false
+    cbot = {}
+    cbot.is_ready = false
+    cbot.is_active = false
 }
 
 // loging in at discord
