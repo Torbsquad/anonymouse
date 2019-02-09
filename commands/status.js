93665060e40ddff92c128e4594e18566f9ad1f89
floofyboi <47008528+floofyboi@users.noreply.github.com>
2019-02-09 16:09:08 +0100

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,3 +1,10 @@
 module.exports = (bot, message, args) => {
-  bot.user.setStatus(args)
+  var args = "grün"
+  var color_to_status = {
+    "grün": "online",
+    "gelb": "idle",
+    "rot": "dnd",
+    "grau": "invisible"
+  }
+  bot.user.setStatus(color_to_status[args])
 }
