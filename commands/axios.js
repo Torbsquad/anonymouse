5438095ac59c676132777a73796215f582584875
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-09 21:18:03 +0100

    added an axios helper-command

---
+++ commands/axios.js
@@ -0,0 +1,10 @@
+module.exports = async (bot, message, args) => {
+  const axios = require("axios")
+
+  var site = await axios.get(args)
+  var sitecontent = site.data
+  JSON.stringify(sitecontent)
+    .split(/(.{0,1000})/)
+    .filter(e=>e)
+    .forEach(v=>message.channel.send(`\`${v}\``))
+}
