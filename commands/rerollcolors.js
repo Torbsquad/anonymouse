9f45e9d49b6584b1ff7dcc61336868e375321b7e
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-02-12 23:03:26 +0100

    added reroll command

---
+++ commands/rerollcolors.js
@@ -0,0 +1,6 @@
+module.exports = (bot, message, args) => {
+  var target_guild = message.guild
+  var target_roles = target_guild.roles.filter(r=>r.name[0]=="ܿ")
+  
+  target_roles.forEach(role=>role.setColor("RANDOM"))
+}
