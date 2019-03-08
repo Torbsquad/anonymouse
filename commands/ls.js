cade24fb4d11712c78db81fd94311be1c8fad136
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-03-08 17:42:09 +0100

    finished ls command

--- commands/ls.js
+++ commands/ls.js
@@ -1,10 +1,18 @@
 const ls = require('lodestonejs')
+const toFetch = ["id", "title", "world", "race", "gender", "clan"]
 
 module.exports = async function(bot, message, args){
-  var char = await als(args)
-  message.reply(char.name)
+  var char = await getlstatus(toFetch, args)
+  message.channel.send(char)
 }
 
-als = (args) => new Promise( (resolve, reject) => {
-  ls(args, (err, data)=>resolve(data))
-})
+async function getlstatus(toFetch, uid){
+  let char = await als(uid)
+  return toFetch.map(f=>`${f}: ${char[f]}`).join('\n')
+}
+
+function als(args){
+  return new Promise((resolve, reject) => {
+    ls(args, (err,data)=>resolve(data))
+  })
+}
