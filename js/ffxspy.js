4656d264184e91d7556a75d0badadaaf2da6c6d4
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-03-08 14:22:24 +0100

    updated to multiple targets

--- js/ffxspy.js
+++ js/ffxspy.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 var ls = require('lodestonejs')
-var random_number = '16466788'
+var random_numbers = [16466788,20859306,20864548,21349090,21853274]
 var bot = ""
 module.exports = main
 
@@ -13,21 +13,31 @@ async function main(b){
 
 async function stalk(channel){
   var richtext = new Discord.RichEmbed();
+  richtext.setColor("b6dd8c")
+  richtext.setThumbnail(data.avatar)
+  richtext.setImage(data.portrait)
+  
   var content = []
   var dinge = ["id","title","world","race","gender","clan"]
+  var memory = JSON.parse(channel.topic)
 
-  ls(random_number, function (err, data) {
-    richtext.setColor("b6dd8c")
-    richtext.setThumbnail(data.avatar)
-    richtext.setImage(data.portrait)
-    for( var ding of dinge ){
-      content.push(`**${ding}**: ${data[ding]||"keins lol"}`)
-    }
-    var result = content.join("\n")
-    if(channel.topic != result){
-      channel.setTopic(result)
-      richtext.addField(data.name,result)
-      channel.send('@everyone',{embed:richtext})
-    }
-  })
+  
+  for(let random_number of random_numbers){
+    ls(random_number,(err, data)=>{
+      for( var ding of dinge ){
+        content.push(`**${ding}**: ${data[ding]||"keins lol"}`)
+      }
+      var result = content.join("\n")
+      
+      if(memory[random_number] != result){
+        memory[random_number] = result
+        channel.setTopic(JSON.stringify(memory))
+        richtext.addField(data.name,result)
+        channel.send('@everyone',{embed:richtext})
+      }
+      
+      message.channel.send(data.name)
+    })
+  }
+  
 }
