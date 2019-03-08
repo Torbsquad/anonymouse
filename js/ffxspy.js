60bb122a085cc9c7945250f432d1696d1c7c869d
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-03-08 17:39:58 +0100

    updated to handle more people

--- js/ffxspy.js
+++ js/ffxspy.js
@@ -1,3 +1,42 @@
+const ls = require('lodestonejs')
+const toSee = [16466788,20859306,20864548,21349090,21853274]
+const toFetch = ["id", "title", "world", "race", "gender", "clan"]
+const Discord = require("discord.js")
+
+module.exports = function(bot){
+  setInterval(main,100000,bot.channels.find(c=>c.name=="👀"),toSee,toFetch)
+}
+
+async function main(channel, target_ids, target_attributes){
+  var memory = JSON.parse(channel.topic);
+  for(let id of target_ids){
+    var char = await getlstatus(target_attributes, id)
+    if( !memory[id] || memory[id] != char.string ){
+      memory[id] = char.string
+
+      var richtext = new Discord.RichEmbed();
+
+      richtext.setThumbnail(char.data.avatar)
+      richtext.setImage(char.data.portrait)
+      richtext.addField(char.data.name,char.string)
+
+      channel.send('@everyone',{embed:richtext})
+    }
+    await channel.setTopic(JSON.stringify(memory))
+  }
+}
+
+async function getlstatus(toFetch, uid){
+  let char = await als(uid)
+  return {string: toFetch.map(f=>`${f}: ${char[f]}`).join('\n'), data: char}
+}
+
+function als(args){
+  return new Promise((resolve, reject) => {
+    ls(args, (err,data)=>resolve(data))
+  })
+}
+
 /*const Discord = require("discord.js")
 var ls = require('lodestonejs')
 var random_numbers = [16466788,20859306,20864548,21349090,21853274]
