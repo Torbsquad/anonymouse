e8eef9c464f76a628b4176a6d5f8f638577a2f9a
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-05-01 23:26:04 +0200

    Create eval.js

---
+++ scripts/eval.js
@@ -0,0 +1,30 @@
+const {Script} = require("vnft-commandhandler")
+
+const eva = new Script()
+
+eva.funct = b => {
+  bot = b
+  b.on("message", (m)=>{
+    try {
+      message = m;
+      bot = b;
+      message.content
+        .split("```")
+        .filter(el => el.startsWith("js\n"))
+        .forEach(el => evalorino(el.substr(3)));
+    } catch (err) {
+      message.reply(err.message);
+    }
+  })
+}
+
+function evalorino(el) {
+  var schleep = "var sleep = time=>new Promise((res,rej)=>{setTimeout(function(){res()},time*1000)})\n";
+  if (el.includes("//sync")) {
+    eval(schleep + el);
+  } else {
+    eval(schleep + `async function main(){try{${el}}catch(err){message.reply(err.message)}}main()`);
+  }
+}
+
+module.exports = eva
