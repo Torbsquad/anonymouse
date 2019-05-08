2fad4c89811efd519c1643b2040ba12fbc57cc3d
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-05-08 22:01:54 +0200

    made sure image hash is string

--- scripts/monkaS.js
+++ scripts/monkaS.js
@@ -23,7 +23,7 @@ monkaScript.funct = (bot) => {
     for(let image of images){
       let urlsnippet = image.match(/\/[0-9]*?\/[0-9]*?\/.*?$/)
       if( urlsnippet ){
-        let hash = (await axios.get("https://untitled-p9bey7ap3m46.runkit.sh/"+urlsnippet[0])).data
+        let hash = (await axios.get("https://untitled-p9bey7ap3m46.runkit.sh/"+urlsnippet[0])).data.toString()
         console.log(hash)
         if ( monkaHash.includes(hash) ) {
           monkaCount++
