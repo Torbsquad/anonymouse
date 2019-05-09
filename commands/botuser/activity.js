10d9d8799cdca6f1e9990205b66c5cbb66e3a473
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-05-09 22:17:11 +0200

    added activity alias and userwhitelist for vonfriedricht

--- commands/botuser/activity.js
+++ commands/botuser/activity.js
@@ -2,8 +2,12 @@ const { Command } = require("vnft-commandhandler");
 
 const activity = new Command();
 activity.name = "setActivity";
+activity.addAlias("activity");
+
 activity.funct = (bot, message, args) => {
   bot.user.setActivity(args);
 };
+
+activity.addUserWhitelist(u => u.id == "397063436049186818");
 
 module.exports = activity;
