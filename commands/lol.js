526b23e59b26f04e7e03c075a6a1385196ab8427
ultrigeon <ultrigeon@pm.me>
2019-05-13 14:42:03 -0400

    added test-command for image response

---
+++ commands/lol.js
@@ -0,0 +1,60 @@
+const Canvas = require("canvas")
+const Discord = require("discord.js")
+const { Command } = require("vnft-commandhandler");
+
+// Pass the entire Canvas object because you'll need to access its width, as well its context
+const applyText = (canvas, text) => {
+	const ctx = canvas.getContext('2d');
+
+	// Declare a base size of the font
+	let fontSize = 70;
+
+	do {
+		// Assign the font to the context and decrement it so it can be measured again
+		ctx.font = `${fontSize -= 10}px sans-serif`;
+		// Compare pixel width of the text to the canvas minus the approximate avatar size
+	} while (ctx.measureText(text).width > canvas.width - 300);
+
+	// Return the result to use in the actual canvas
+	return ctx.font;
+};
+
+const lol = new Command();
+lol.funct = async (bot, message, args) => {
+	const member = bot.users.find(u=>u.username=="VonFriedricht");
+	const channel = bot.channels.find(c=>c.id=="265207562360717329");
+	if (!channel) return;
+
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	// Slightly smaller text placed above the member's display name
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	// Add an exclamation point here and below
+	ctx.font = applyText(canvas, `${member.username}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.username}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(member.displayAvatarURL);
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+	console.log(123)
+	channel.send(`Welcome to the server, ${member}!`, attachment);
+}
+
+module.exports = lol;
