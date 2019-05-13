fee9d4685b07eb1fb17271276b949bde629efbb4
floofyboi <47008528+floofyboi@users.noreply.github.com>
2019-05-13 21:17:45 +0200

    Delete say.js

--- commands/chat/say.js
+++
@@ -1,9 +0,0 @@
-const { Command } = require("vnft-commandhandler");
-
-const say = new Command();
-say.name = "say";
-say.funct = (bot, message, args) => {
-  message.channel.send(args);  
-};
-
-module.exports = say;
