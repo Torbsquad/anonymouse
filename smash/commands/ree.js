1bc33465ee01db35eaef156d406cd9f8f4b44452
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-20 23:07:45 +0200

    moved the stagecollection to own file

--- smash/commands/ree.js
+++ smash/commands/ree.js
@@ -1,21 +1,8 @@
 const SmashScreen = require("../class/SmashScreen");
-const StageCollection = require("../class/StageCollection");
 const Player = require("../class/Player");
 const { Command } = require("vnft-commandhandler");
 
-const starterStages = new StageCollection("smash/img/stages/starters/");
-starterStages.addStage("Letzte Station", "letztestation.png");
-starterStages.addStage("Lylat-Patrouille", "lylatpatrouille.png");
-starterStages.addStage("Pokémon Stadium 2", "pokemonstadium2.png");
-starterStages.addStage("Schlachtfeld", "schlachtfeld.png");
-starterStages.addStage("Smash-Stadt", "smashstadt.png");
-
-const counterpickStages = new StageCollection("smash/img/stages/counterpicks/");
-counterpickStages.addStage("Kalos-Pokémon-Liga", "kalopokemonliga.png");
-counterpickStages.addStage("Stadt & Großstadt", "townandcity.png");
-counterpickStages.addStage("Einall-Pokémon-Liga", "einallpokemonliga.png");
-counterpickStages.addStage("Yoshi’s Island", "yoshisisland.png");
-counterpickStages.addStage("Yoshi’s Story", "yoshisstory.png");
+const stages = require("../stages");
 
 const a = new Command();
 a.name = "a";
@@ -31,9 +18,9 @@ a.funct = async (bot, message, args) => {
   screen.addPlayer(player1);
   screen.addPlayer(player2);
 
-  screen.addStages(starterStages);
+  screen.addStages(stages.starters);
   if (player1.score != 0 || player2.score != 0) {
-    screen.addStages(counterpickStages);
+    screen.addStages(stages.counterpicks);
   }
 
   message.channel.send("", await screen.draw());
