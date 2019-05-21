f21d155dfcfed62e0b2b362a89b3fc8064f1a978
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-05-21 15:09:14 +0200

    Create analyseEmoji.js

---
+++ commands/analyseEmoji.js
@@ -0,0 +1,10 @@
+const { Command } = require("vnftjs");
+const analyse = require("../../js/emojiAnalysis");
+
+const analyseEmoji = new Command();
+analyseEmoji.name = "analyse";
+analyseEmoji.funct = async (bot, message, args) => {
+  message.channel.send("`"+JSON.stringify(await analyse(args))+"`")
+};
+
+module.exports = analyseEmoji;
