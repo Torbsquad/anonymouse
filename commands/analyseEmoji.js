f75c899f12a2f6704acab901d3e413568be84576
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-21 16:12:46 +0200

    checking if analysis is empty

--- commands/analyseEmoji.js
+++ commands/analyseEmoji.js
@@ -4,7 +4,13 @@ const analyse = require("../js/emojiAnalysis");
 const analyseEmoji = new Command();
 analyseEmoji.name = "analyse";
 analyseEmoji.funct = async (bot, message, args) => {
-  message.channel.send("`"+JSON.stringify(await analyse(args))+"`")
+  let analysis = await analyse(args);
+  if(analysis){
+    message.channel.send("`"+JSON.stringify(analysis)+"`")
+  }
+  else{
+    message.channel.send("i dont get any results on that")
+  }
 };
 
 module.exports = analyseEmoji;
