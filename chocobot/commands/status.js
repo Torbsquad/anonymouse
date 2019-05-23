be84a428e6cf74aa3cd1e6893df3a9e7854adee0
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-23 11:14:47 +0200

    fixed and reorganised the translation

--- chocobot/commands/status.js
+++ chocobot/commands/status.js
@@ -3,17 +3,18 @@ const { Command } = require("vnftjs")
 const status = new Command()
 status.name = "setStatus"
 
-const translation = {
-    "red": "dnd",
-    "green": "online",
-    "yellow": "away"
-}
+const translations = [
+  { status: "dnd", alias: ["red", "busy"] },
+  { status: "online", alias: ["green", ""] },
+  { status: "idle", alias: ["yellow", "away", "afk"] }
+]
 
 status.funct = (bot, message, args) => {
-    if( translation[args] ){
-        args = translation[args]
-    }
-    bot.user.setStatus(args)
+  let translation = translations.find(t => t.alias.includes(args))
+  if (translation) {
+    args = translation.status
+  }
+  bot.user.setStatus(args)
 }
 
 module.exports = status
