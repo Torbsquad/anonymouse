4d43bca337038248156f802c261f9f1c4629acad
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-26 19:39:45 +0200

    set prefix to dot

--- chocobot/main.js
+++ chocobot/main.js
@@ -2,6 +2,7 @@ const { CommandHandler } = require("vnftjs");
 const path = require("path");
 
 const bot = new CommandHandler();
+bot.prefix = "dot";
 
 bot.loadCommands(path.join(__dirname, "commands"));
 bot.login(process.env.chocobot);
