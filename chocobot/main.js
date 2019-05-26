7f7731a45bea0b93caadf1726b66d1f953fcbafb
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-05-26 23:56:06 +0200

    enabled help comamnd

--- chocobot/main.js
+++ chocobot/main.js
@@ -4,5 +4,7 @@ const path = require("path");
 const client = new CommandHandler();
 client.prefix = "beemoviescript";
 
+client.enableHelp();
+
 client.loadCommands(path.join(__dirname, "commands"));
 client.login(process.env.chocobot);
