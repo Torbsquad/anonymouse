1a2597b2be1d3c9d4e3fd4be1bf3302feefd190e
floofyboi <47008528+floofyboi@users.noreply.github.com>
2019-05-26 19:51:54 +0200

    Update main.js

--- chocobot/main.js
+++ chocobot/main.js
@@ -2,7 +2,7 @@ const { CommandHandler } = require("vnftjs");
 const path = require("path");
 
 const client = new CommandHandler();
-client.prefix = "dot";
+client.prefix = "beemoviescript";
 
 client.loadCommands(path.join(__dirname, "commands"));
 client.login(process.env.chocobot);
