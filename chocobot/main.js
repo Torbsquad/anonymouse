6f7b234cff24f687cb18451545e29c5ec0471f2a
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-26 19:40:12 +0200

    renamed variable bot to client

--- chocobot/main.js
+++ chocobot/main.js
@@ -1,8 +1,8 @@
 const { CommandHandler } = require("vnftjs");
 const path = require("path");
 
-const bot = new CommandHandler();
-bot.prefix = "dot";
+const client = new CommandHandler();
+client.prefix = "dot";
 
-bot.loadCommands(path.join(__dirname, "commands"));
-bot.login(process.env.chocobot);
+client.loadCommands(path.join(__dirname, "commands"));
+client.login(process.env.chocobot);
