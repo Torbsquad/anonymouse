afb49386981a748a422e6f152b1d741c18ab1d25
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-05-26 19:43:49 +0200

    added sayd command

---
+++ chocobot/commands/sayd.js
@@ -0,0 +1,12 @@
+const { Command } = require("vnftjs");
+
+const sayd = new Command();
+sayd.name = "sayd";
+sayd.description = "Replies with the text after sayd and __tries__ to delete the source-message";
+
+sayd.funct = (client, message, args) => {
+  message.channel.send(args);
+  message.delete();
+};
+
+module.exports = sayd;
