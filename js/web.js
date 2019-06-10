46478971d2b4f974e2b2109e51324c708af2bc77
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-10 23:50:15 +0200

    removed old web

--- js/web.js
+++
@@ -1,5 +0,0 @@
-express = require("express");
-PORT = process.env.PORT || 5000;
-express()
-  .get("/", (req, res) => res.send("<pre>o/</pre>"))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
