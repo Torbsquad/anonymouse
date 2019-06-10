fd3de8dceb827ede1b60ec5013a49a952e01e91e
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-10 23:58:32 +0200

    changed chocobot color

--- chocobot/scripts/ffxspy.js
+++ chocobot/scripts/ffxspy.js
@@ -39,7 +39,7 @@ test.funct = async bot => {
 
     if (hasChanged) {
       let response = new RichEmbed();
-      response.setColor("YELLOW");
+      response.setColor("FFFF99");
 
       const TARGET = [].concat(
         TARGET_ATTRIBUTES,
