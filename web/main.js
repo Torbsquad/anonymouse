a22c069244d8332bc86d865ef744a4b20ea7d929
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-11 22:52:01 +0200

    removed express-subdomain from production

--- web/main.js
+++ web/main.js
@@ -7,28 +7,11 @@ const { server } = require("vnft-tools");
 const app = express();
 app.get("/", function(req, res) {
   res.send("Hello I am running in anonymouse!");
+  console.log(res)
 });
 
 app.use(express.static(__dirname + '/public', {
   extensions: ['html']
 }));
-
-
-// api fun start
-const subdomain = require("express-subdomain")
-var router = express.Router();
- 
-router.get('/', function(req, res) {
-    res.send('Welcome to our API!');
-});
- 
-router.get('/users', function(req, res) {
-    res.json([
-        { name: "Brian" }
-    ]);
-});
-
-app.use(subdomain('api', router));
-// api fun end
 
 server(app,key,cert);
