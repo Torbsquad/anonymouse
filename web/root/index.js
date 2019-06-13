2db36222335ddd835e1d81d2f7dc3c5b327f71f8
ultrigeon <ultrigeon@pm.me>
2019-06-13 06:37:23 -0400

    added public folder

--- web/root/index.js
+++ web/root/index.js
@@ -10,4 +10,6 @@ app.get('/chart.js', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..',  'node_modules', 'chart.js', 'dist', 'Chart.js'))
 })
 
+app.use(express.static('public'))
+
 module.exports = app;
