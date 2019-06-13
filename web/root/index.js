124c94df257a47db36e3b028af3fc6212e4fea80
ultrigeon <ultrigeon@pm.me>
2019-06-13 06:41:03 -0400

    let ignore .html extension at /public

--- web/root/index.js
+++ web/root/index.js
@@ -10,6 +10,8 @@ app.get('/chart.js', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..',  'node_modules', 'chart.js', 'dist', 'Chart.js'))
 })
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public', {
+  extensions: ['html']
+}));
 
 module.exports = app;
--- web/rooter.js
+++ web/rooter.js
@@ -1,13 +1,13 @@
 const cert = process.env.certificate;
 const key = process.env.privatekey;
 
-const express = require('express')
-const vhost = require('vhost')
+const express = require("express");
+const vhost = require("vhost");
 
-const { server } = require("vnft-tools")
+const { server } = require("vnft-tools");
 
-const app = express()
-app.use(vhost('vnft.cc', require('./root')))
-app.use(vhost('api.vnft.cc', require('./api')))
+const app = express();
+app.use(vhost("vnft.cc", require("./root")));
+app.use(vhost("api.vnft.cc", require("./api")));
 
 server(app, key, cert);
