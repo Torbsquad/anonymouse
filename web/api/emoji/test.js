afbb636bce81790ed1ce5a0abcca901e9c449070
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-14 21:58:10 +0200

    added prototype for vnftjs but just for express

---
+++ web/api/emoji/test.js
@@ -0,0 +1,9 @@
+let reee = {}
+
+reee.name = "/emoji";
+
+reee.get = (app, req, res) => {
+    res.json({ status: "OK!" });
+}
+
+module.exports = reee;
--- web/api/index.js
+++ web/api/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const { fetchJS } = require("vnft-tools")
+const path = require("path")
 
 app.get("/", (req, res) => {
   res.json({ status: "OK!" });
 });
+
+let files = fetchJS(path.join(__dirname,"emoji"));
+for(let file of files){
+  if( file.name && file.get ){
+    app.get(file.name, file.get);
+  }
+}
 
 module.exports = app;
