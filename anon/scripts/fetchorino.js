e96f58e46244756b630fab437137bc42540a3c03
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-07-10 14:05:35 +0200

    Update fetchorino.js

--- anon/scripts/fetchorino.js
+++ anon/scripts/fetchorino.js
@@ -5,32 +5,6 @@ const axios = require('axios')
 const fetchorino = new Script()
 fetchorino.interval = 25000
 
-async function fetchMessages(channel_id, message_id) {
-  let channel = bot.channels.find(c => c.id === channel_id)
-  let messages = await channel.fetchMessages({ after: message_id, limit: 100 })
-  pg.query(`
-    UPDATE EMOJI_CRAWL SET 
-      LAST_MESSAGE_ID = $(lastMessage),
-      LAST_FETCH = NOW()
-      WHERE CHANNEL_ID = $(channelId)
-    `,
-    {
-      lastMessage: messages.first().id,
-      channelId: channel_id
-    }
-  )
-  return messages
-}
-
-async function readMessage(message) {
-  if (message.content) {
-    let emojis = message.content.match(/<(a|):.*?:.*?>/g)
-    for (let emoji of emojis) {
-      await axios.get(`https://api.vnft.cc/emoji/add/${emoji}`)
-    }
-  }
-}
-
 fetchorino.funct = async bot => {
   const ergebnis = await pg.one(`
     SELECT * FROM EMOJI_CRAWL 
@@ -38,10 +12,39 @@ fetchorino.funct = async bot => {
       LIMIT 1
   `)
 
-  let messages = await fetchMessages(ergebnis.channel_id, ergebnis.last_message_id)
-  for (let message of messages) {
-    await readMessage(message)
+  let channel = bot.channels.find(c => c.id == ergebnis.channel_id)
+  let messages = await channel.fetchMessages({
+    after: ergebnis.last_message_id,
+    limit: 100
+  })
+
+  if (messages.size) {
+    await pg.query(
+      `
+      UPDATE EMOJI_CRAWL SET 
+        LAST_MESSAGE_ID = $(lastMessage),
+        LAST_FETCH = NOW()
+        WHERE CHANNEL_ID = $(channelId)
+      `,
+      {
+        lastMessage: messages.first().id,
+        channelId: ergebnis.channel_id
+      }
+    )
+    for (let message of messages.array()) {
+      console.log(message.content)
+      if (message.content) {
+        let emojis = message.content.match(/<(a|):.*?:.*?>/g)
+        if(!Array.isArray(emojis)){
+          emojis = [emojis]
+        }
+        for (let emoji of emojis) {
+          await axios.get(`https://api.vnft.cc/emoji/add/${emoji}`)
+        }
+      }
+    }
   }
+
 }
 
 module.exports = fetchorino
