664e80ae14e44faa0ac2a53cf7e0bdb575bc6123
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-07-10 13:26:28 +0200

    Update fetchorino.js

--- anon/scripts/fetchorino.js
+++ anon/scripts/fetchorino.js
@@ -2,7 +2,7 @@ const { Script } = require('vnftjs')
 const pg = require('../../db')
 
 const fetchorino = new Script()
-fetchorino.interval = 10000
+fetchorino.interval = 25000
 
 async function fetchMessages(channel_id, message_id) {
   let channel = bot.channels.find(c => c.id === channel_id)
@@ -21,11 +21,11 @@ async function fetchMessages(channel_id, message_id) {
   return messages
 }
 
-function readMessage(message) {
+async function readMessage(message) {
   if (message.content) {
     let emojis = message.content.match(/<(a|):.*?:.*?>/g)
     for (let emoji of emojis) {
-      axios.get(`https://api.vnft.cc/emoji/add/${emoji}`)
+      await axios.get(`https://api.vnft.cc/emoji/add/${emoji}`)
     }
   }
 }
@@ -39,7 +39,7 @@ fetchorino.funct = async bot => {
 
   let messages = await fetchMessages(ergebnis.channel_id, ergebnis.last_message_id)
   for (let message of messages) {
-    readMessage(message)
+    await readMessage(message)
   }
 }
 
