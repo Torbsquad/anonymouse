59d853bcd40d24b1f2dfaf9a4e947ef65281c120
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-07-11 14:22:15 +0200

    Create crawlstatus.js

---
+++ web/api/emoji/crawlstatus.js
@@ -0,0 +1,10 @@
+const pg = require('../../../db')
+
+const { Site } = require('vnft-tools')
+const status = new Site('/emoji/status')
+
+status.get = async (req, res) => {
+  res.json(await pg.any('SELECT * FROM EMOJI_CRAWL'))
+}
+
+module.exports = status
