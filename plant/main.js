8ab61b12bab38031d15bfb6a5124d4ce48096383
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-07-12 18:02:15 +0200

    added scripts folder

--- plant/main.js
+++ plant/main.js
@@ -8,4 +8,5 @@ plant.helpColor = 'PURPLE'
 plant.prefix = 'p'
 
 plant.loadCommands(path.join(__dirname, 'commands'))
+plant.loadCommands(path.join(__dirname, 'scripts'))
 plant.login(process.env.plant)
