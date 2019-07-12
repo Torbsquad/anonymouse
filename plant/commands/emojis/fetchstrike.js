46f4bb0e11a33ad9752d41d10e2166271a6c7d15
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-07-13 01:05:25 +0200

    Create fetchstrike.js

---
+++ plant/commands/emojis/fetchstrike.js
@@ -0,0 +1,63 @@
+const { Command } = require('vnftjs')
+let fetchStrike = new Command()
+fetchStrike.name = "fetch"
+
+async function getChannelPointer(channelid){
+    try {
+        let queryResult = await pg.one(`
+            select last_message_id from emoji_crawl 
+            where channel_id=$(channel) limit 1
+        `,{channel:channelid})
+        return queryResult["last_message_id"]
+    }
+    catch(err){
+        return "0"
+    }
+}
+
+async function setChannelPointer(channelid, messageid){
+    let query =`
+        UPDATE emoji_crawl SET last_message_id=$(message) WHERE channel_id=$(channel);
+        INSERT INTO emoji_crawl (channel_id, last_message_id)
+           SELECT $(channel), $(message)
+           WHERE NOT EXISTS (SELECT 1 FROM emoji_crawl WHERE channel_id=$(channel));
+    `
+    let options = {
+        channel: channelid,
+        message: messageid
+    }
+    await pg.query(query, options) 
+}
+
+async function tick(channelId){
+    let channel = client.channels.find(c => c.id == channelId)
+    let channelPointer = await getChannelPointer(channelId)
+    let messages = await channel.fetchMessages({after:channelPointer})
+    if(messages.size == 0){
+        return false
+    }
+    channelPointer = messages.first().id
+    setChannelPointer(channelId, channelPointer)
+
+    for( let message of messages ){
+        console.log(message)
+        if(message.content){
+            let emojis = message.content.match(/<(a|):.*?:.*?>/g)
+            if(!emojis){
+                break
+            }
+            if(!Array.isArray(emojis)){
+                emojis = [emojis]
+            }
+            for(let emoji of emojis){
+                //axios.get(`https://api.vnft.cc/emoji/add/${emoji}`)
+            }
+        }
+    }
+}
+
+fetchStrike.funct = (b,m,a) => {
+  tick(a)
+}
+
+module.exports = fetchStrike
