9d5c6298e6128ce293737ff51057f56e04eb5ddf
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-07-12 19:22:05 +0200

    Create paged.js

---
+++ web/api/emoji/paged.js
@@ -0,0 +1,16 @@
+const pg = require('../../../db')
+
+const { Site } = require('vnft-tools')
+const all = new Site('/emoji/page/:page')
+
+all.get = async (req, res) => {
+  let query = `SELECT * FROM EMOJIS OFFSET $(offset) LIMIT $(limit)`
+  let options = {
+    offset: Number(req.params.page)*100,
+    limit: 100
+  }
+
+  res.json(await pg.any(query, options))
+}
+
+module.exports = all
