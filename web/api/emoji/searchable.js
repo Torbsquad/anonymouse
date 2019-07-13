3061673a4119f93cf718ada2991bd524ee693e6c
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-13 23:36:06 +0200

    added order by points

--- web/api/emoji/searchable.js
+++ web/api/emoji/searchable.js
@@ -3,13 +3,22 @@ const { Site } = require('vnft-tools')
 
 const search = new Site('/emoji/search/:search/:page')
 search.get = async (req, res) => {
-  let query = `SELECT * FROM EMOJIS WHERE name like $(search) OFFSET $(offset) LIMIT $(limit)`
+  
+  let query = `
+    SELECT * FROM EMOJIS 
+      WHERE name like $(search) 
+      ORDER BY points desc 
+      OFFSET $(offset) LIMIT $(limit)
+    `
+  
   let options = {
     offset: Number(req.params.page)*100,
     limit: 100,
     search: `%${req.params.search}%`
   }
+  
   res.json(await pg.any(query, options))
+  
 }
 
 module.exports = search
