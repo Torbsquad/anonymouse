733514615704f7c8664c83ef468a0991fdc44f89
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-13 23:49:54 +0200

    added axios example to vnft.cc/axios

--- db.js
+++ db.js
@@ -1,3 +1,2 @@
 const postgres = require('pg-promise')()(process.env.DATABASE_URL + '?ssl=true')
-
 module.exports = postgres
--- web/root/index.js
+++ web/root/index.js
@@ -10,6 +10,10 @@ app.get('/chart.js', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'node_modules', 'chart.js', 'dist', 'Chart.js'))
 })
 
+app.get('/axios.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'node_modules', 'axios', 'dist', 'axios.js'))
+})
+
 app.use(
   express.static(__dirname + '/public', {
     extensions: ['html'],
