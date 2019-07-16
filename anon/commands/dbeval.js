b7b98cbe4bd2794fa29eee0d78e8ff66afbbfd82
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-16 15:22:43 +0200

    Create dbeval.js

---
+++ anon/commands/dbeval.js
@@ -0,0 +1,15 @@
+const pg = require('../../db')
+
+const { Command } = require('vnftjs')
+
+const dbeval = new Command()
+dbeval.name = "db"
+dbeval.addUserWhitelist(u => u.id == "397063436049186818")
+
+dbeval.funct = async (bot, message, args) => {
+    let ergebnis = await pg.any(args)
+    ergebnis = JSON.stringify(ergebnis,null,2)
+    message.channel.send("```"+ergebnis+"```")
+}
+
+module.exports = dbeval
