52c18d3c7aa1687547e29d23ce9079a795811c72
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-07-18 15:21:47 +0200

    Create last.js

---
+++ web/api/emoji/last.js
@@ -0,0 +1,21 @@
+const pg = require('../../../db')
+
+const { Site } = require('vnft-tools')
+const last = new Site('/emoji/last/:page')
+
+last.get = async (req, res) => {
+  let offsetQuery = 'select count(*) from emojis'
+  let offsetResult = await pg.one(offsetQuery)
+  let offset = offsetResult.count
+  
+  let mainQuery = 'select * from emojis offset $(offset) limit $(limit)'
+  let mainOptions = {
+    offset: offset - Number(req.params.page) * 100,
+    limit: 100,
+  }
+  let mainResult = await pg.any(mainQuery, mainOptions)
+
+  res.json(mainResult)
+}
+
+module.exports = last
