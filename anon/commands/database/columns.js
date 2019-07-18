9b86d36af534475f2520378bd03f58c404e0a726
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-18 06:56:56 +0200

    prettier

--- anon/commands/database/columns.js
+++ anon/commands/database/columns.js
@@ -7,36 +7,40 @@ command.name = 'columns'
 command.addUserWhitelist(u => u.id == '397063436049186818')
 
 command.funct = async (bot, message, args) => {
-  let results = await pg.any(`SELECT column_name, data_type FROM information_schema.columns WHERE table_schema='public' and table_name=$1`, args)
+  let results = await pg.any(
+    `SELECT column_name, data_type FROM information_schema.columns WHERE table_schema='public' and table_name=$1`,
+    args,
+  )
 
   function fillup(text, length) {
-    while(text.length < length){text += " "}
+    while (text.length < length) {
+      text += ' '
+    }
     return text
   }
 
-  function repeattext(text, length){
-    let result = ""
-    while(length-->0){result += text}
+  function repeattext(text, length) {
+    let result = ''
+    while (length-- > 0) {
+      result += text
+    }
     return result
   }
 
-  let namesLength = Math.max(...results.map(t=>t.column_name.length))
-  let typesLength = Math.max(...results.map(t=>t.data_type.length))
+  let namesLength = Math.max(...results.map(t => t.column_name.length))
+  let typesLength = Math.max(...results.map(t => t.data_type.length))
 
   let response = [
-      `╒═${repeattext("═",namesLength)}══╤═${repeattext("═",typesLength)}══╕`,
-      `│ ${fillup("name",namesLength)}  │ ${fillup("type",typesLength)}  │`,
-      `╞═${repeattext("═",namesLength)}══╪═${repeattext("═",typesLength)}══╡`
+    `╒═${repeattext('═', namesLength)}══╤═${repeattext('═', typesLength)}══╕`,
+    `│ ${fillup('name', namesLength)}  │ ${fillup('type', typesLength)}  │`,
+    `╞═${repeattext('═', namesLength)}══╪═${repeattext('═', typesLength)}══╡`,
   ]
-  for(let result of results){
-      response.push(`│ ${fillup(result.column_name,namesLength)}  │ ${fillup(result.data_type,typesLength)}  │`)
+  for (let result of results) {
+    response.push(`│ ${fillup(result.column_name, namesLength)}  │ ${fillup(result.data_type, typesLength)}  │`)
   }
-  response.push(`└─${repeattext("─",namesLength)}──┴─${repeattext("─",typesLength)}──┘`)
+  response.push(`└─${repeattext('─', namesLength)}──┴─${repeattext('─', typesLength)}──┘`)
 
-  
-  message.channel.send('```' + response.join("\n") + '```')
+  message.channel.send('```' + response.join('\n') + '```')
 }
-
-
 
 module.exports = command
