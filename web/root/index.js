11bebd1e5663d77bbb3749d91427e252090e4a0e
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-20 22:07:18 +0200

    allowing crossorigin at root

--- web/root/index.js
+++ web/root/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('Hallo Welt!')
 })
