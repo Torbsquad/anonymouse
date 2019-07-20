c8e0f30e489b0848b83369afdaba78e01c6f331f
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-20 21:56:14 +0200

    moved test-assets to us

--- web/root/public/phaser/main.js
+++ web/root/public/phaser/main.js
@@ -17,7 +17,7 @@ var config = {
 var game = new Phaser.Game(config)
 
 function preload() {
-  this.load.setBaseURL('http://labs.phaser.io')
+  this.load.setBaseURL('https://vnft.cc/phaser')
 
   this.load.image('sky', 'assets/skies/space3.png')
   this.load.image('logo', 'assets/sprites/phaser3-logo.png')
