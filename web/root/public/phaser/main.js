fbaa458cbc2f5b33a71b7ce5d8965452be9800bc
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-20 23:48:48 +0200

    removing baseurl

--- web/root/public/phaser/main.js
+++ web/root/public/phaser/main.js
@@ -1,6 +1,4 @@
 function preload() {
-  this.load.setBaseURL('https://vnft.cc/phaser')
-
   this.load.image('sky', 'assets/skies/space3.png')
   this.load.image('logo', 'assets/sprites/phaser3-logo.png')
   this.load.image('red', 'assets/particles/red.png')
