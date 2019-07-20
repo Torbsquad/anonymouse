0118653bbd17e6e4ac5880c0ed027e6caf03aae0
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-20 22:04:23 +0200

    moved config to index.html

--- web/root/public/phaser/main.js
+++ web/root/public/phaser/main.js
@@ -1,21 +1,3 @@
-var config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 },
-    },
-  },
-  scene: {
-    preload: preload,
-    create: create,
-  },
-}
-
-var game = new Phaser.Game(config)
-
 function preload() {
   this.load.setBaseURL('https://vnft.cc/phaser')
 
