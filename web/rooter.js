7f33571abfa35c691efd612209e7ea6be2eef3cb
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-21 09:44:41 +0200

    added core-folder

--- web/rooter.js
+++ web/rooter.js
@@ -10,5 +10,6 @@ const app = express()
 app.use(vhost('vnft.cc', require('./root')))
 app.use(vhost('api.vnft.cc', require('./api')))
 app.use(vhost('localhost', require('./root')))
+app.use(vhost('anon-spirit.herokuapp.com', require('./core')))
 
 server(app, key, cert)
