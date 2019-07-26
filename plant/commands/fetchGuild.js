88360d7f51fbd07df0b734aec2fd4532b6f8f748
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-26 20:07:35 +0200

    added fetchguild

---
+++ plant/commands/fetchGuild.js
@@ -0,0 +1,80 @@
+const { Command } = require('vnftjs')
+const pg = require('../../db')
+const axios = require('axios')
+const { sleep } = require('vnft-tools')
+let fetchStrike = new Command()
+fetchStrike.name = 'fetch'
+let client
+
+async function getChannelPointer(channelid) {
+  try {
+    let queryResult = await pg.one(
+      `
+          select last_message_id from emoji_crawl 
+          where channel_id=$(channel) limit 1
+        `,
+      { channel: channelid },
+    )
+    return queryResult['last_message_id']
+  } catch (err) {
+    return '0'
+  }
+}
+
+async function setChannelPointer(channelid, messageid) {
+  let query = `
+        UPDATE emoji_crawl SET last_message_id=$(message) WHERE channel_id=$(channel);
+        INSERT INTO emoji_crawl (channel_id, last_message_id)
+           SELECT $(channel), $(message)
+           WHERE NOT EXISTS (SELECT 1 FROM emoji_crawl WHERE channel_id=$(channel));
+    `
+  let options = {
+    channel: channelid,
+    message: messageid,
+  }
+  await pg.query(query, options)
+}
+
+async function tick(channelId) {
+  let channel = client.channels.find(c => c.id == channelId)
+  let channelPointer = await getChannelPointer(channelId)
+  let messages = await channel.fetchMessages({ after: channelPointer })
+  if (messages.size == 0) {
+    return false
+  }
+  channelPointer = messages.first().id
+  setChannelPointer(channelId, channelPointer)
+
+  for (let message of messages) {
+    if (message.content) {
+      let emojis = message.content.match(/<(a|):.*?:.*?>/g)
+      if (!emojis) {
+        break
+      }
+      if (!Array.isArray(emojis)) {
+        emojis = [emojis]
+      }
+      for (let emoji of emojis) {
+        console.log('found:', emoji)
+        axios.get(`https://api.vnft.cc/emoji/add/${emoji}`)
+      }
+    }
+  }
+}
+
+fetchStrike.funct = async (b, m, a) => {
+  client = b
+
+  let guild = b.guilds.find(g => g.id === a)
+  if (!guild.available) return false
+  let channels = guild.channels
+
+  for (let channel of channels) {
+    if (channel.type == 'text') {
+      tick(channel.id)
+      sleep(1000)
+    }
+  }
+}
+
+module.exports = fetchStrike
