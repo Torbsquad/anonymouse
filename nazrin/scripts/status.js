36599318cfb3657b40714dcb8f6cf4decf97a1ea
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-27 23:57:41 +0200

    added activity to nazrin

--- nazrin/scripts/status.js
+++ nazrin/scripts/status.js
@@ -2,7 +2,8 @@ const { Script } = require('vnftjs')
 
 const script = new Script()
 script.funct = bot => {
-  bot.user.setStatus("idle")
+  bot.user.setStatus('idle')
+  bot.user.setActivity('vnft.cc')
 }
 
 module.exports = script
