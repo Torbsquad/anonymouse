86b45c1168777ffc713ea82bfc638afe2c6d32d6
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-27 23:46:13 +0200

    fixed quotes

--- nazrin/commands/invite.js
+++ nazrin/commands/invite.js
@@ -3,7 +3,7 @@ const { Command } = require('vnftjs')
 const command = new Command()
 command.name = 'invite'
 command.funct = (bot, message, args) => {
-  message.reply("https://discordapp.com/oauth2/authorize?client_id=553204804843864084&scope=bot&permissions=1073744896")
+  message.reply('https://discordapp.com/oauth2/authorize?client_id=553204804843864084&scope=bot&permissions=1073744896')
 }
 
 module.exports = command
