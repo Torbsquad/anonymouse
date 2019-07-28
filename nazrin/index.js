2ebe2a7250d9fa56c7b84558b9f8dcf5fd5129b8
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-28 23:38:19 +0200

    added load from scriptfolder

--- nazrin/index.js
+++ nazrin/index.js
@@ -3,6 +3,6 @@ const bot = new CommandHandler()
 const path = require('path')
 
 bot.loadCommands(path.join(__dirname, 'commands'))
-//bot.loadScripts(path.join(__dirname, 'scripts'))
+bot.loadScripts(path.join(__dirname, 'scripts'))
 
 bot.login(process.env.nazrin)
