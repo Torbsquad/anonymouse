aea34cbc2a23aed5a627a8c811607072b612e572
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-30 23:25:18 +0200

    added description

--- anon/commands/botuser/activity.js
+++ anon/commands/botuser/activity.js
@@ -3,6 +3,7 @@ const { Command } = require('vnftjs')
 const command = new Command()
 command.name = 'setActivity'
 command.addAlias('activity')
+command.description = "Setting Activity from bot"
 
 command.funct = (bot, message, args) => {
   bot.user.setActivity(args)
