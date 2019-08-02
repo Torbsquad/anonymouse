82df876fdde82d86d53dc96248b808dde25c2ebd
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-02 21:25:38 +0200

    reorderen server start

--- web/rooter.js
+++ web/rooter.js
@@ -7,14 +7,14 @@ var path = require('path')
 
 const { server } = require('vnft-tools')
 
+const Server = server(app, key, cert)
+
+global.io = require('socket.io').listen(Server)
+global.root_directory = path.resolve('../')
+
 const app = express()
 app.use(vhost('vnft.cc', require('./root')))
 app.use(vhost('socket.vnft.cc', require('./socket')))
 app.use(vhost('api.vnft.cc', require('./api')))
 app.use(vhost('localhost', require('./root')))
 app.use(vhost('anon-spirit.herokuapp.com', require('./core')))
-
-const Server = server(app, key, cert)
-
-global.io = require('socket.io').listen(Server)
-global.root_directory = path.resolve('../')
