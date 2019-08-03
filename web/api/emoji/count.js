a884128144d052f3a441c1f5760ecea344a90588
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-08-03 16:17:57 +0200

    changed db name

--- web/api/emoji/count.js
+++ web/api/emoji/count.js
@@ -4,7 +4,7 @@ const { Site } = require('vnft-tools')
 const count = new Site('/emoji/count')
 
 count.get = async (req, res) => {
-  let query = await pg.one('SELECT COUNT(*) FROM EMOJIS')
+  let query = await pg.one('SELECT COUNT(*) FROM EMOJIS2')
   res.send(query.count)
 }
 
