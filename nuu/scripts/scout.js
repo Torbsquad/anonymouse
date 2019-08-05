281d1ba49056271e7fd836f2e126280cbc01a1ba
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-08-06 00:38:27 +0200

    full rework

--- nuu/scripts/scout.js
+++ nuu/scripts/scout.js
@@ -1,23 +1,23 @@
 const { Script } = require('vnftjs')
 const axios = require('axios')
 
-const scout = new Script()
+const script = new Script()
 
-scout.funct = b => {
-  b.on('message', async message => {
-    if (message.content) {
-      let emojis = message.content.match(/<(a|):.*?:.*?>/g)
-      if (!emojis) {
-        return false
-      }
-      if (!Array.isArray(emojis)) {
-        emojis = [emojis]
-      }
-      for (let emoji of emojis) {
-        await axios.get(`https://api.vnft.cc/emoji/add/${emoji}`)
-      }
-    }
-  })
+async function getEmojis(message){
+  message = message.match(/<(a|):.*?:.*?>/g)
+  if (!message) {
+    return false
+  }
+  if (!Array.isArray(message)) {
+    message = [message]
+  }
+  for (let emoji of message) {
+    await axios.get(`https://api.vnft.cc/emoji/add/${emoji}`)
+  }   
 }
 
-module.exports = scout
+script.funct = (bot) => {
+  bot.on("raw",e=>{ if(e.t == "MESSAGE_CREATE") getEmojis(e.d.content) })
+}
+
+module.exports = script
