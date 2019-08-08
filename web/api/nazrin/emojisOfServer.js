9ae466b3f06b59330617b74f532b6edadb3186d9
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-08 22:34:05 +0200

    translation to valid properties

--- web/api/nazrin/emojisOfServer.js
+++ web/api/nazrin/emojisOfServer.js
@@ -2,11 +2,12 @@ const nazrin = require('../../../nazrin')
 
 const { Site } = require('vnft-tools')
 const site = new Site('/nazrin/serverEmojis/:id')
+const emojiproperties = ["animated", "id", "name", "url"]
 
 site.get = async (req, res) => {
     try{
         let server = nazrin.guilds.find(g=>g.id==req.params.id)
-        let emojis = server.emojis//.map(e=>e.toString())
+        let emojis = server.emojis.map(e=>emojiproperties.map(p=>e[p]))
         res.json(emojis)
     }
     catch(err){
