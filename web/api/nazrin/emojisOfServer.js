13444f4a0ea5e81c05010ba8a8cc8f1733f60d23
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-10 20:06:51 +0200

    hardcoded emojiproperties-array inside code

--- web/api/nazrin/emojisOfServer.js
+++ web/api/nazrin/emojisOfServer.js
@@ -3,12 +3,18 @@ const analyse = require('../../../js/emojiAnalysis')
 
 const { Site } = require('vnft-tools')
 const site = new Site('/nazrin/serverEmojis/:id')
-const emojiproperties = ['animated', 'id', 'name', 'url']
 
 site.get = async (req, res) => {
   try {
     let server = nazrin.guilds.find(g => g.id == req.params.id)
-    let emojis = server.emojis.map(e => emojiproperties.map(f => e[f]))
+    let emojis = server.emojis.map(e => {
+      return {
+        animated: e.animated,
+        id: e.id,
+        name: e.name,
+        url: e.url,
+      }
+    })
     res.json(emojis)
   } catch (err) {
     res.json(err)
