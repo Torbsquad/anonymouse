0f39920a2d29da66d3404002f429d3aa17318d6e
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-12 00:04:51 +0200

    made getter work with emojis2-database

--- web/api/nazrin/getFromBackup.js
+++ web/api/nazrin/getFromBackup.js
@@ -5,14 +5,11 @@ const { Site } = require('vnft-tools')
 const site = new Site('/nazrin/backup/get/:serverid')
 
 site.get = async (req, res) => {
-  let emojisInBackup = await pg.any(
-    `
-    SELECT hash, name FROM emoji_server_backup
+  let emojisInBackup = await pg.any(`
+      SELECT emojis2.hash, emojis2.name, emojis2.url FROM emoji_server_backup
+      LEFT JOIN emojis2 ON emoji_server_backup.hash = emojis2.hash 
       where server_id = $(server_id)
-    `,
-    {
-      server_id: req.params.serverid,
-    },
+    `, { server_id: req.params.serverid }
   )
 
   res.json(emojisInBackup)
