e4d4982ce1dd4e71e11550002deacb20b77cfc94
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-11 23:32:48 +0200

    made emoji_server_backup insert api interface

--- web/api/nazrin/putInBackup.js
+++ web/api/nazrin/putInBackup.js
@@ -1,12 +1,29 @@
-/*
-`
-INSERT INTO emoji_server_backup
-    ("hash", "name", "server_id")
-values
-    (
-     $(hash),
-     $(name),
-     $(server_id)
-    )
-`
-*/
+const pg = require('../../../db')
+const analyse = require('../../../js/emojiAnalysis')
+
+const { Site } = require('vnft-tools')
+const add = new Site('/nazrin/backup/:hash/:name/:serverid')
+
+add.get = async (req, res) => {
+
+  await pg.query(`
+    INSERT INTO emoji_server_backup
+        ("hash", "name", "server_id")
+    values
+        (
+         $(hash),
+         $(name),
+         $(server_id)
+        )
+    `,
+    {
+        hash: req.params.hash,
+        name: req.params.name,
+        server_id: req.params.serverid
+    },
+  )
+
+  res.json({ done: true })
+}
+
+module.exports = add
