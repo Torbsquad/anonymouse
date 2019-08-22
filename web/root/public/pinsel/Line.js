1aa9f097d2f4acc5227c6fca568ee7bd7596b49d
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-22 12:00:00 +0200

    init Line-Class

---
+++ web/root/public/pinsel/Line.js
@@ -0,0 +1,26 @@
+class Line{
+    constructor(){
+        this.dots = []
+    }
+    addDot(x,y,size){
+        this.dots.push([x,y,size])
+    }
+    drawInCanvas(canvas){
+        var context = canvas.getContext("2d")
+        for(var dot of this.dots){
+            var x = dot[0]
+            var y = dot[1]
+            var size = dot[2]
+            point(x,y,size,context)
+        }
+    }
+}
+
+function point(x, y, size, context){
+    context.lineCap = 'round';
+    context.lineWidth = size
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(x+.1, y+.1);
+    context.stroke();
+}
--- web/root/public/pinsel/main.js
+++ web/root/public/pinsel/main.js
@@ -1,3 +1,12 @@
 function main(){
     console.log("Hi!")
+
+    var c = document.getElementById("leinwand")
+
+    var l = new Line()
+    l.addDot(10+i,10+i,10)
+    for(var i = 0; i < 100; i++ ){
+        l.addDot(10+i,10+i,100-i)
+    }
+    l.drawInCanvas(c)
 }
