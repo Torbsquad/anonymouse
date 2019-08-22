db4765b2f131079262027d44f9f172cf165419e1
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-22 23:13:39 +0200

    br

--- nuu/main.js
+++ nuu/main.js
@@ -1,5 +1,7 @@
 const { CommandHandler } = require('vnftjs')
 const client = new CommandHandler()
+
 client.loadCommands('./nuu/commands')
 client.loadScripts('./nuu/scripts')
+
 client.login(process.env.plant)
