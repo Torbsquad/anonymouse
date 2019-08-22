89b3551e42e0c4b8f50ad782f8b748b4d8a94f37
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-22 23:00:31 +0200

    added whitelist

--- nuu/commands/say.js
+++ nuu/commands/say.js
@@ -1,10 +1,11 @@
 const { Command } = require('vnftjs')
 
-const psay = new Command()
-psay.name = 'say'
+const command = new Command()
+command.name = 'say'
 
-psay.funct = (plant, message, args) => {
+command.addUserWhitelist(u=>u.id=='397063436049186818')
+command.funct = (plant, message, args) => {
   message.channel.send(args)
 }
 
-module.exports = psay
+module.exports = command
