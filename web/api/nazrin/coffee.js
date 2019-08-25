e2844ad8e0ac64318ab78a9332d8117819fae059
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-25 21:21:20 +0200

    letting filter after textchannel

--- web/api/nazrin/coffee.js
+++ web/api/nazrin/coffee.js
@@ -4,7 +4,7 @@ const { Site } = require('vnft-tools')
 const site = new Site('/nazrin/coffee')
 
 site.get = async (req, res) => {
-  let channels = nazrin.channels.find(c => c.guild.id == '254735952969334801')
+  let channels = nazrin.channels.find(c => c.type == 'text' && c.guild.id == '254735952969334801')
   res.json(channels.map(c => c.name))
 }
 
