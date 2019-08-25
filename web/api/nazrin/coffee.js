43d381286d062d25bf60e04677794c8b235ed4c9
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-25 21:27:55 +0200

    return namecollection

--- web/api/nazrin/coffee.js
+++ web/api/nazrin/coffee.js
@@ -4,9 +4,8 @@ const { Site } = require('vnft-tools')
 const site = new Site('/nazrin/coffee')
 
 site.get = async (req, res) => {
-  let channels = nazrin.channels.find(c => c.type == 'text' && c.guild.id == '254735952969334801')
-  console.log(channels)
-  res.json('ok')
+  let channels = nazrin.channels.filter(c => c.type == 'text' && c.guild.id == '254735952969334801')
+  res.json(channels.map(c=>c.name))
 }
 
 module.exports = site
