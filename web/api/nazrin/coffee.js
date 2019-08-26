1cca2d82dac588361d40c2565bcd62fe73558cab
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-08-26 10:36:11 +0200

    changed memberlist to member-username-list

--- web/api/nazrin/coffee.js
+++ web/api/nazrin/coffee.js
@@ -5,9 +5,10 @@ const site = new Site('/nazrin/coffee')
 
 site.get = async (req, res) => {
   let targetGuild = nazrin.guilds.find(g => g.id == '254735952969334801')
-  let members = targetGuild.members
+  
+  let members = targetGuild.members.map(member => member.user.username)
+                                        
   let channels = targetGuild.channels.filter(c => c.type == 'text')
-
   let rights = channels.map(channel => {
     return {
       name: channel.name,
