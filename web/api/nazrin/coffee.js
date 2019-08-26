3fe7b86109a938a52aacab1cbadd0f811179269a
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-26 09:57:58 +0200

    fixed missing bracket

--- web/api/nazrin/coffee.js
+++ web/api/nazrin/coffee.js
@@ -4,22 +4,22 @@ const { Site } = require('vnft-tools')
 const site = new Site('/nazrin/coffee')
 
 site.get = async (req, res) => {
-  let targetGuild = nazrin.guilds.find( g => g.id == '254735952969334801' )
+  let targetGuild = nazrin.guilds.find(g => g.id == '254735952969334801')
   let members = targetGuild.members
-  let channels = targetGuild.channels.filter( c => c.type == 'text' )
-  
-  let response = channels.map( channel => {
+  let channels = targetGuild.channels.filter(c => c.type == 'text')
+
+  let response = channels.map(channel => {
     return {
       name: channel.name,
-      userViewRights: channel.members.map( member => {
+      userViewRights: channel.members.map(member => {
         return {
           name: member.user.username,
-          canView: channel.permissionsFor(member).has("VIEW_CHANNEL") 
+          canView: channel.permissionsFor(member).has('VIEW_CHANNEL')
         }
-      )
+      })
     }
   })
-  
+
   res.json(response)
 }
 
