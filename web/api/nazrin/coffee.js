b0d8bab89cee506bfc179d876714161dc2515dce
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-26 09:41:28 +0200

    prepared viewright-display

--- web/api/nazrin/coffee.js
+++ web/api/nazrin/coffee.js
@@ -5,7 +5,19 @@ const site = new Site('/nazrin/coffee')
 
 site.get = async (req, res) => {
   let channels = nazrin.channels.filter(c => c.type == 'text' && c.guild.id == '254735952969334801')
-  res.json(channels.map(c=>c.name))
+  let response = channels.map(channel=>{
+    return {
+      name: channel.name,
+      viewRights: "tbd"
+    }
+  })
+  res.json(response)
 }
 
 module.exports = site
+
+/*```js
+let permissions = message.channel.permissionsFor(message.member)
+let re = permissions.has("VIEW_CHANNEL")
+message.reply(re)
+```*/
