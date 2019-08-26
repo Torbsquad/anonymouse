ea247c1e8f55977bd3800e0ca52d976b6e0fa44e
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-26 08:08:48 +0200

    changed prefix to _

--- nazrin/index.js
+++ nazrin/index.js
@@ -2,6 +2,7 @@ const { CommandHandler } = require('vnftjs')
 const bot = new CommandHandler()
 const path = require('path')
 
+bot.prefix = '_'
 bot.loadCommands(path.join(__dirname, 'commands'))
 bot.loadScripts(path.join(__dirname, 'scripts'))
 
