b522c45e9123d5e32a905f88ccc23ae9d1df7ad5
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-29 11:27:13 +0200

    added code for defaultBlock and defaultBlockTable

--- web/root/public/blind/script.js
+++ web/root/public/blind/script.js
@@ -60,6 +60,7 @@ class Table {
     this.grid = grid
     this.el = document.createElement('table')
     this.el.className = 'noselect'
+    this.cellToggle = function(event){console.log(1)}
     this.update()
   }
 
@@ -67,6 +68,11 @@ class Table {
     while (this.grid.length < y) this.grid.push([])
     while (this.grid[y].length < x) this.grid[y].push(0)
     this.grid[y][x] = value
+  }
+
+  getTile(x, y) {
+    if (this.grid.length <= y || this.grid[y].length <= x) return 0
+    return this.grid[y][x]
   }
 
   resizeTo(width, height) {
@@ -86,19 +92,25 @@ class Table {
   }
 
   update() {
-    this.el.innerHTML = this.grid
-      .map(
-        (row, y) =>
-          `<tr>${row
-            .map(
-              (cell, x) =>
-                `<td data-x="${x}" data-y="${y}" class="cell" onmousemove="toggle(event, this)" onmousedown="toggle(true, this)" class="noselect" style="background:${
-                  cell ? 'black' : 'white'
-                }"></td>`,
-            )
-            .join('')}</tr>`,
-      )
-      .join('')
+    var tableBody = document.createElement("tbody")
+    for(let y = 0; y < this.grid.length; y++){
+      var tr = document.createElement("tr")
+      for(let x = 0; x < this.grid[y].length; x++){
+        var cell = this.grid[y][x]
+        var td = document.createElement("td")
+        td.className = "noselect cell"
+        td.onmousedown = this.cellToggle
+        td.onmousemove = this.cellToggle
+        td.setAttribute("data-x", x)
+        td.setAttribute("data-y", y)
+        td.innerHTML = cell
+        td.style.background = cell ? 'black' : 'white'
+        tr.appendChild(td)
+      }
+      tableBody.appendChild(tr)
+    }
+    this.el.innerHTML = ""
+    this.el.appendChild(tableBody)
   }
 }
 
@@ -148,14 +160,59 @@ function tableLoad() {
   output.innerHTML = grid.toBlind()
 }
 
+
 var output = document.getElementById('output')
 var drawBoard = document.getElementById('drawBoard')
+var defaultBlock = document.getElementById('defaultBlock')
 
-var grid = new Grid([[0, 0], [0, 1], [1, 0, 1, 1], [0, 0], [0, 1, 1, 1, 0, 1]])
+grid = new Grid([[0, 0], [0, 1], [1, 0, 1, 1], [0, 0], [0, 1, 1, 1, 0, 1]])
 
 var table = new Table(grid.grid)
 table.el.className = 'noselect bordered'
-resize()
+table.cellToggle = function(event){
+  if ( event.buttons != 0 ) {
+    if (DRAWMODE == -1 || event.type == "mousedown") {
+      DRAWMODE = (this.innerHTML == 0) * 1
+    }
+    this.innerHTML = DRAWMODE
+    this.style.background = DRAWMODE ? 'black' : 'white'
+    table.setTile(this.getAttribute('data-x'), this.getAttribute('data-y'), DRAWMODE)
+    grid.grid = table.grid
+    output.innerHTML = grid.toBlind()
+  } else {
+    DRAWMODE = -1
+  }
+}
+
+defaultBlockTable = new Table([[0,0],[0,0],[0,0],[0,0]])
+defaultBlockTable.el.className = 'noselect bordered'
+defaultBlockTable.value = function(x=0, y=0) {
+  return this.getTile(x, y) * 1 +
+    this.getTile(x, y + 1) * 2 +
+    this.getTile(x, y + 2) * 4 +
+    this.getTile(x + 1, y) * 8 +
+    this.getTile(x + 1, y + 1) * 16 +
+    this.getTile(x + 1, y + 2) * 32 +
+    this.getTile(x, y + 3) * 64 +
+    this.getTile(x + 1, y + 3) * 128
+}
+defaultBlockTable.cellToggle = function(event) {
+  if ( event.buttons != 0 ) {
+    if (DRAWMODE == -1 || event.type == "mousedown") {
+      DRAWMODE = (this.innerHTML == 0) * 1
+    }
+    this.innerHTML = DRAWMODE
+    this.style.background = DRAWMODE ? 'black' : 'white'
+    defaultBlockTable.setTile(this.getAttribute('data-x'), this.getAttribute('data-y'), DRAWMODE)
+    grid.emptychar = defaultBlockTable.value()
+    output.innerHTML = grid.toBlind()
+  } else {
+    DRAWMODE = -1
+  }
+}
+
+defaultBlockTable.update()
 
 output.innerHTML = grid.toBlind()
 drawBoard.appendChild(table.el)
+defaultBlock.appendChild(defaultBlockTable.el)
