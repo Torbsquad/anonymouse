38cd3f153857a9e151a38b42b9ef52b6d2d1e3d3
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-31 21:36:00 +0200

    added random api interface

---
+++ web/api/emoji/random.js
@@ -0,0 +1,16 @@
+const pg = require('../../../db')
+
+const { Site } = require('vnft-tools')
+const random = new Site('/emoji/random/:page')
+
+random.get = async (req, res) => {
+  let mainQuery = 'select * from emojis2 where random() < 0.01 limit $(limit)'
+  let mainOptions = {
+    limit: 100,
+  }
+  let mainResult = await pg.any(mainQuery, mainOptions)
+
+  res.json(mainResult)
+}
+
+module.exports = random
