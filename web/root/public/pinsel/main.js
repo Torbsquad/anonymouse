de09ce120076471c53e9279cea3aef845231e134
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-08-31 12:00:00 +0200

    prettier

--- web/root/public/pinsel/main.js
+++ web/root/public/pinsel/main.js
@@ -1,12 +1,12 @@
-function main(){
-    console.log("Hi!")
+function main() {
+  console.log('Hi!')
 
-    var c = document.getElementById("leinwand")
+  var c = document.getElementById('leinwand')
 
-    var l = new Line()
-    l.addDot(10+i,10+i,10)
-    for(var i = 0; i < 100; i++ ){
-        l.addDot(10+i,10+i,100-i)
-    }
-    l.drawInCanvas(c)
+  var l = new Line()
+  l.addDot(10 + i, 10 + i, 10)
+  for (var i = 0; i < 100; i++) {
+    l.addDot(10 + i, 10 + i, 100 - i)
+  }
+  l.drawInCanvas(c)
 }
