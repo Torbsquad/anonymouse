a54dc3d1b8bc7b86acc66a9810a03e76a3ffccd3
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-02 11:14:56 +0200

    Create input.js

---
+++ web/root/public/socket/input.js
@@ -0,0 +1,12 @@
+var inputs = {}
+function isPressed(key) {
+  return inputs[key] == true
+}
+
+document.addEventListener("keydown",event => {
+  inputs[event.key] = true;
+})
+
+document.addEventListener("keyup",event => {
+  inputs[event.key] = false;
+})
