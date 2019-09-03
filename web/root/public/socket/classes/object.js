dcfa3ccad1d6c91e4b71476fc6b532984842df53
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-03 11:09:54 +0200

    added static draw method

--- web/root/public/socket/classes/object.js
+++ web/root/public/socket/classes/object.js
@@ -1,5 +1,14 @@
 class GameObject {
-  constructor() {
+  constructor(x, y, width, height) {
     objects.push(this)
+
+    this.x = x || Math.floor(Math.random()*100)
+    this.y = y || Math.floor(Math.random()*100)
+    this.width = width || Math.floor(Math.random()*100)
+    this.height = height || Math.floor(Math.random()*100)
+  }
+
+  static draw_(ctx, obj = this, canvas, camera) {
+    ctx.fillRect(obj.x + canvas.width/2 - camera.x, obj.y + canvas.height/2 - camera.y, obj.width, obj.height)
   }
 }
