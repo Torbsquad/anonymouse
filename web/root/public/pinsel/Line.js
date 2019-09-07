8ac60f175fb00f851484f0faec3d5d02aa6a197e
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-07 12:00:00 +0200

    adding linecolor

--- web/root/public/pinsel/Line.js
+++ web/root/public/pinsel/Line.js
@@ -1,26 +1,25 @@
 class Line {
   constructor() {
     this.dots = []
+    this.color = "red"
   }
   addDot(x, y, size) {
     this.dots.push([x, y, size])
   }
   drawInCanvas(canvas) {
     var context = canvas.getContext('2d')
+    context.strokeStyle = this.color
     for (var dot of this.dots) {
       var x = dot[0]
       var y = dot[1]
       var size = dot[2]
-      point(x, y, size, context)
+      context.lineCap = 'round'
+      context.lineWidth = size
+      context.beginPath()
+      context.moveTo(x, y)
+      context.lineTo(x + 0.1, y + 0.1)
+      context.stroke()
     }
   }
 }
 
-function point(x, y, size, context) {
-  context.lineCap = 'round'
-  context.lineWidth = size
-  context.beginPath()
-  context.moveTo(x, y)
-  context.lineTo(x + 0.1, y + 0.1)
-  context.stroke()
-}
