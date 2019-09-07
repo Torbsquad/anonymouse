922d4c873834c836ecf5caad23d3a8361128038f
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-07 23:56:33 +0200

    Update getChunk.js

--- web/api/socket/getChunk.js
+++ web/api/socket/getChunk.js
@@ -4,14 +4,14 @@ const { Site } = require('vnft-tools')
 const count = new Site('/socket/getChunk/:x/:y')
 
 count.get = async (req, res) => {
-  let query = await pg.any(`
+  let chunk = await pg.any(`
     SELECT chunk FROM socket_chunks where x = $(x) and y = $(y)
   `,
   {
     x: Number(req.params.x), 
     y: Number(req.params.y)
   })
-  res.json({status:"ok"})
+  res.json(chunk)
 }
 
 module.exports = count
