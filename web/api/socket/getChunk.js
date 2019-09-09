65a3e689ac10a5954d7ba1308d57cd33d8b7709e
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-09 04:21:47 +0200

    added erroroutput if no chunk found

--- web/api/socket/getChunk.js
+++ web/api/socket/getChunk.js
@@ -4,16 +4,15 @@ const { Site } = require('vnft-tools')
 const site = new Site('/socket/getChunk/:x/:y')
 
 site.get = async (req, res) => {
-  let chunk = await pg.one(
-    `
-    SELECT chunk FROM socket_chunks where x = $(x) and y = $(y) limit 1
-  `,
-    {
+  try {
+    let chunk = await pg.one(`SELECT chunk FROM socket_chunks where x = $(x) and y = $(y) limit 1`, {
       x: Number(req.params.x),
       y: Number(req.params.y),
-    },
-  )
-  res.json(chunk)
+    })
+    res.json(chunk)
+  } catch (err) {
+    res.json({ error: 'no chunk found' })
+  }
 }
 
 module.exports = site
