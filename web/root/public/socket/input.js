0b9ee95d5838d530227ec39526eb24ad30817073
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-10 10:08:23 +0200

    Update input.js

--- web/root/public/socket/input.js
+++ web/root/public/socket/input.js
@@ -1,12 +1,20 @@
 var inputs = {}
+var pointer = {x:0, y:0}
 function isPressed(key) {
   return inputs[key] == true
 }
 
-document.addEventListener('keydown', event => {
-  inputs[event.key] = true
-})
+function initInputs(){
+  document.addEventListener('keydown', event => {
+    inputs[event.key] = true
+  })
 
-document.addEventListener('keyup', event => {
-  inputs[event.key] = false
-})
+  document.addEventListener('keyup', event => {
+    inputs[event.key] = false
+  })
+
+  canvas.addEventListener('mousemove', event => {
+    pointer.x = event.layerX
+    pointer.y = event.layerY
+  })
+}
