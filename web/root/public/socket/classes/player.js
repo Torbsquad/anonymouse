c657c8c9c559e94179c57714f72fe341996dc16a
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-10 12:22:34 +0200

    added collision detection with simple test

--- web/root/public/socket/classes/player.js
+++ web/root/public/socket/classes/player.js
@@ -7,34 +7,26 @@ class Player extends GameObject {
     this.height = 40
     this.movingSince = 0
 
+    this.moving = false
+
+    this.hitboxX = 0
+    this.hitboxY = 20
+    this.hitboxWidth = 20
+    this.hitboxHeight = 20
+
     this.speed = 4
   }
 
   logic() {
-    let moving = false
-    if (inputs['ArrowUp'] || inputs['w']) {
-      this.y -= this.speed
-      moving = true
-    }
-    if (inputs['ArrowDown'] || inputs['s']) {
-      this.y += this.speed
-      moving = true
-    }
-    if (inputs['ArrowLeft'] || inputs['a']) {
-      this.x -= this.speed
-      moving = true
-    }
-    if (inputs['ArrowRight'] || inputs['d']) {
-      this.x += this.speed
-      moving = true
-    }
+    this.moving = false
+    this.moveX(this.speed*(!(inputs['ArrowLeft']||inputs['a'])-!(inputs['ArrowRight']||inputs['d'])))
+    this.moveY(this.speed*(!(inputs['ArrowUp']||inputs['w'])-!(inputs['ArrowDown']||inputs['s'])))
 
-    if (moving == false) {
+    if (this.moving == false) {
       this.movingSince = 0
     } else if (this.movingSince == 0) {
       this.movingSince = new Date()
     }
-    // console.log(inputs)
   }
 
   toData() {
@@ -46,4 +38,33 @@ class Player extends GameObject {
       animFrame: this.movingSince != 0 ? Math.round((new Date() - this.movingSince) / 200) + 1 : 0,
     }
   }
+
+  moveX(value) {
+    if(value != 0) {
+      this.x += value
+      if(value>0)while(this.inGridCollision())this.x--
+      if(value<0)while(this.inGridCollision())this.x++
+    }
+  }
+
+  moveY(value) {
+    if(value != 0) {
+      this.y += value
+      if(value>0)while(this.inGridCollision())this.y--
+      if(value<0)while(this.inGridCollision())this.y++
+    }
+  }
+
+  inGridCollision() {
+    let hbx = this.x + this.hitboxX
+    let hby = this.y + this.hitboxY
+    
+    let left = Math.floor(hbx/32)
+    let top = Math.floor(hby/32)
+    let right = Math.ceil((hbx+this.hitboxWidth)/32)
+    let down = Math.ceil((hby+this.hitboxHeight)/32)
+
+    return left < 0 || top < 0 || down > 16 || right > 16
+  }
+
 }
