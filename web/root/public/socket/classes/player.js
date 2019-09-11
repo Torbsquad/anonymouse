10942346a30c2c78cf888c4af01f1bb47f43c0b8
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-11 18:26:18 +0200

    prettier

--- web/root/public/socket/classes/player.js
+++ web/root/public/socket/classes/player.js
@@ -19,8 +19,8 @@ class Player extends GameObject {
 
   logic() {
     this.moving = false
-    this.moveX(this.speed*(!(inputs['ArrowLeft']||inputs['a'])-!(inputs['ArrowRight']||inputs['d'])))
-    this.moveY(this.speed*(!(inputs['ArrowUp']||inputs['w'])-!(inputs['ArrowDown']||inputs['s'])))
+    this.moveX(this.speed * (!(inputs['ArrowLeft'] || inputs['a']) - !(inputs['ArrowRight'] || inputs['d'])))
+    this.moveY(this.speed * (!(inputs['ArrowUp'] || inputs['w']) - !(inputs['ArrowDown'] || inputs['s'])))
 
     if (this.moving == false) {
       this.movingSince = 0
@@ -40,29 +40,29 @@ class Player extends GameObject {
   }
 
   moveX(value) {
-    if(value != 0) {
+    if (value != 0) {
       this.x += value
-      if(value>0)while(this.inCollision())this.x--
-      if(value<0)while(this.inCollision())this.x++
+      if (value > 0) while (this.inCollision()) this.x--
+      if (value < 0) while (this.inCollision()) this.x++
     }
   }
 
   moveY(value) {
-    if(value != 0) {
+    if (value != 0) {
       this.y += value
-      if(value>0)while(this.inCollision())this.y--
-      if(value<0)while(this.inCollision())this.y++
+      if (value > 0) while (this.inCollision()) this.y--
+      if (value < 0) while (this.inCollision()) this.y++
     }
   }
 
   inCollision() {
-    function posToTile(y, x){
-      let cx = Math.floor(x/16)
-      let cy = Math.floor(y/16)
-      let tx = x - cx*16
-      let ty = y - cy*16
-      let chunk = chunks[cx+','+cy]
-      if(chunk && chunk.grid){
+    function posToTile(y, x) {
+      let cx = Math.floor(x / 16)
+      let cy = Math.floor(y / 16)
+      let tx = x - cx * 16
+      let ty = y - cy * 16
+      let chunk = chunks[cx + ',' + cy]
+      if (chunk && chunk.grid) {
         return idToTile[chunk.grid[ty][tx]][3]
       }
       return true
@@ -70,11 +70,11 @@ class Player extends GameObject {
 
     let hbx = this.x + this.hitboxX
     let hby = this.y + this.hitboxY
-    
-    let left = Math.floor(hbx/32)
-    let top = Math.floor(hby/32)
-    let right = Math.floor((hbx+this.hitboxWidth)/32)
-    let down = Math.floor((hby+this.hitboxHeight)/32)
+
+    let left = Math.floor(hbx / 32)
+    let top = Math.floor(hby / 32)
+    let right = Math.floor((hbx + this.hitboxWidth) / 32)
+    let down = Math.floor((hby + this.hitboxHeight) / 32)
 
     let topLeft = posToTile(top, left)
     let topRight = posToTile(top, right)
@@ -83,5 +83,4 @@ class Player extends GameObject {
 
     return topLeft || topRight || downRight || downLeft
   }
-
 }
