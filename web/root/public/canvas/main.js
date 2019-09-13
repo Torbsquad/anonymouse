1175a5dbc627d96f268e25ceab39a0fd785c8dfc
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-13 12:00:00 +0200

    adding mousemove response

--- web/root/public/canvas/main.js
+++ web/root/public/canvas/main.js
@@ -2,10 +2,17 @@ var canvas = document.getElementById('canvas')
 var cx = canvas.getContext('2d')
 
 function main() {
+  resizeWindow()
   cx.fillRect(0, 0, 100, 100)
 }
 
-window.addEventListener('resize', event => {
+function resizeWindow() {
   canvas.height = window.innerHeight - 1
   canvas.width = window.innerWidth - 1
+}
+
+window.addEventListener('resize', resizeWindow)
+
+document.addEventListener('mousemove', event => {
+  cx.fillRect(event.layerX, event.layerY, 2, 2)
 })
