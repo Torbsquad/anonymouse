30090798040ed180bcc11050dfd2aaf2171ae4c4
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-25 12:00:00 +0200

    added Lineadder to Leinwand

--- web/root/public/pinsel/Leinwand.js
+++ web/root/public/pinsel/Leinwand.js
@@ -4,9 +4,12 @@ class Leinwand {
     this.canvas.width = '500'
     this.canvas.height = '500'
     this.canvas.style.background = 'white'
-
+    this.lines = []
     this.canvas.onclick = function() {
       console.log(1 + 1)
     }
+  }
+  addLine(line) {
+    this.lines.push(line)
   }
 }
