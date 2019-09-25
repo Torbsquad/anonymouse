bc1163003188868c98fd0072e9eb5ba30fb4411d
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-25 20:53:52 +0200

    prettier

--- anon/commands/canvas/filter.js
+++ anon/commands/canvas/filter.js
@@ -24,50 +24,34 @@ function filter(cxd) {
   let avgs = new Array()
   let result = new Array()
   for (let i = 0; i < subPixelCount; i += 4) {
-    let r = cxd.data[i]**2
-    let g = cxd.data[i + 1]**2
-    let b = cxd.data[i + 2]**2
-    let v = Math.sqrt(r + g + b) 
+    let r = cxd.data[i] ** 2
+    let g = cxd.data[i + 1] ** 2
+    let b = cxd.data[i + 2] ** 2
+    let v = Math.sqrt(r + g + b)
     avgs.push(v)
     result.push(0)
   }
 
   let filters = []
-  
-  filters.push([
-    [-1, 0, 1],
-    [-2, 0, 2],
-    [-1, 0, 1]
-  ])
-  filters.push([
-    [1, 0, -1],
-    [2, 0, -2],
-    [1, 0, -1]
-  ])
-  filters.push([
-    [-1, -2, -1],
-    [0, 0, 0],
-    [1, 2, 1]
-  ])
-  filters.push([
-    [1, 2, 1],
-    [0, 0, 0],
-    [-1, -2, -1]
-  ])
 
-  for( let y = 0; y < cxd.height; y++ ){
-    for( let x = 0; x < cxd.width; x++ ){
-      for( let filter of filters ){
+  filters.push([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+  filters.push([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+  filters.push([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+  filters.push([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+
+  for (let y = 0; y < cxd.height; y++) {
+    for (let x = 0; x < cxd.width; x++) {
+      for (let filter of filters) {
         var fsum = 0
-        for( let subY = -1; subY <= 1; subY++ ){
-          for( let subX = -1; subX <= 1; subX++ ){
-            let v = getPixel(cxd, avgs, x+subX, y+subY)
-            fsum += filter[subY+1][subX+1]*v
+        for (let subY = -1; subY <= 1; subY++) {
+          for (let subX = -1; subX <= 1; subX++) {
+            let v = getPixel(cxd, avgs, x + subX, y + subY)
+            fsum += filter[subY + 1][subX + 1] * v
           }
         }
         let i = getIndex(cxd, x, y)
-        if(fsum > 0){
-          result[i] += fsum/4
+        if (fsum > 0) {
+          result[i] += fsum / 4
         }
       }
     }
