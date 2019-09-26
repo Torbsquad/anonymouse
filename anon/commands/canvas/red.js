706b3b1536815c9f90bcfdd75b04ade597e560d7
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-26 21:27:58 +0200

    Create red.js

---
+++ anon/commands/canvas/red.js
@@ -0,0 +1,31 @@
+const Canvas = require('canvas')
+const Discord = require('discord.js')
+const { Command } = require('vnftjs')
+
+const command = new Command()
+command.name = 'red'
+
+command.funct = async (bot, message, args) => {
+  const userImg = await Canvas.loadImage(message.author.avatarURL)
+
+  const canvas = Canvas.createCanvas(userImg.width, userImg.height)
+  const ctx = canvas.getContext('2d')
+  ctx.drawImage(userImg, 0, 0, canvas.width, canvas.height)
+
+  let cxd = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  ctx.putImageData(filter(cxd), 0, 0)
+
+  const attachment = new Discord.Attachment(canvas.toBuffer(), `user ${message.author.username}.png`)
+  message.channel.send(``, attachment)
+}
+
+function filter(cxd) {
+  let subPixelCount = Object.keys(cxd.data).length
+  for (let i = 0; i < subPixelCount; i += 4) {
+    cxd.data[i + 1] = 0
+    cxd.data[i + 2] = 0
+  }
+  return cxd
+}
+
+module.exports = command
