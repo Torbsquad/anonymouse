ecfdf1290c4527124b8c54bf98ad4d98b2229939
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-26 21:23:04 +0200

    Create basecolors.js

---
+++ anon/commands/canvas/basecolors.js
@@ -0,0 +1,35 @@
+const Canvas = require('canvas')
+const Discord = require('discord.js')
+const { Command } = require('vnftjs')
+
+const command = new Command()
+command.name = 'basecolors'
+
+command.funct = async (bot, message, args) => {
+  const userImg = await Canvas.loadImage(message.author.avatarURL)
+  
+  const canvas = Canvas.createCanvas(userImg.width, userImg.height)
+  const ctx = canvas.getContext('2d')
+  ctx.drawImage(userImg, 0, 0, canvas.width, canvas.height)
+
+  let cxd = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  ctx.putImageData(greyscale(cxd), 0, 0)
+
+  const attachment = new Discord.Attachment(canvas.toBuffer(), `user ${message.author.username}.png`)
+  message.channel.send(``, attachment)
+}
+
+function greyscale(cxd) {
+  let subPixelCount = Object.keys(cxd.data).length
+  for (let i = 0; i < subPixelCount; i += 4) {
+    let r = cxd.data[i]
+    let g = cxd.data[i + 1]
+    let b = cxd.data[i + 2]
+    cxd.data[i] = r > g + b ? r : 0
+    cxd.data[i + 1] = g > b + r ? g : 0
+    cxd.data[i + 2] = b > r + g ? b : 0
+  }
+  return cxd
+}
+
+module.exports = command
