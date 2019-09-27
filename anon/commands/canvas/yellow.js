24977f9d09836dcb07bd26f8648dc3f8c8da3d4f
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-27 23:52:43 +0200

    casting green value to number

--- anon/commands/canvas/yellow.js
+++ anon/commands/canvas/yellow.js
@@ -22,7 +22,7 @@ command.funct = async (bot, message, args) => {
 function filter(cxd) {
   let subPixelCount = Object.keys(cxd.data).length
   for (let i = 0; i < subPixelCount; i += 4) {
-    cxd.data[i + 1] = cxd.data[i + 1] / 2
+    cxd.data[i + 1] = Number(cxd.data[i + 1]) / 2
     cxd.data[i + 2] = 0
   }
   return cxd
