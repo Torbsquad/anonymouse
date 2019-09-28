303c12e394415fcb5e2ea0c16bad7d0fb5904912
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-28 12:53:17 +0200

    implementing loadCanvasByImage and applyFilterToImageData

--- anon/commands/canvas/basecolors.js
+++ anon/commands/canvas/basecolors.js
@@ -1,4 +1,6 @@
-const Canvas = require('canvas')
+const loadCanvasByImage = require("./helperFunctions/loadCanvasByImage")
+const applyFilterToImageData = require("./helperFunctions/applyFilterToImageData")
+
 const Discord = require('discord.js')
 const { Command } = require('vnftjs')
 
@@ -6,14 +8,8 @@ const command = new Command()
 command.name = 'basecolors'
 
 command.funct = async (bot, message, args) => {
-  const userImg = await Canvas.loadImage(message.author.avatarURL)
-
-  const canvas = Canvas.createCanvas(userImg.width, userImg.height)
-  const ctx = canvas.getContext('2d')
-  ctx.drawImage(userImg, 0, 0, canvas.width, canvas.height)
-
-  let cxd = ctx.getImageData(0, 0, canvas.width, canvas.height)
-  ctx.putImageData(greyscale(cxd), 0, 0)
+  const canvas = await loadCanvasByImage(message.author.avatarURL)
+  applyFilterToImageData(canvas, greyscale)
 
   const attachment = new Discord.Attachment(canvas.toBuffer(), `user ${message.author.username}.png`)
   message.channel.send(``, attachment)
