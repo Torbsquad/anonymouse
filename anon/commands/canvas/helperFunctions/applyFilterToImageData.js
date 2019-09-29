5fe5b726f58caf3d3e05cff1ed5863375a86ff76
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-29 07:11:39 +0200

    prettier

--- anon/commands/canvas/helperFunctions/applyFilterToImageData.js
+++ anon/commands/canvas/helperFunctions/applyFilterToImageData.js
@@ -1,4 +1,4 @@
-function applyFilterToImageData(canvas, funct){
+function applyFilterToImageData(canvas, funct) {
   const ctx = canvas.getContext('2d')
   const cxd = ctx.getImageData(0, 0, canvas.width, canvas.height)
   ctx.putImageData(funct(cxd), 0, 0)
