c519c5bebfaa17ee7756e15831e1c5a49c2b8e63
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-09-30 23:57:20 +0200

    Create green.js

---
+++ anon/commands/canvas/green.js
@@ -0,0 +1,27 @@
+const loadCanvasByImage = require('./helperFunctions/loadCanvasByImage')
+const applyFilterToImageData = require('./helperFunctions/applyFilterToImageData')
+
+const Discord = require('discord.js')
+const { Command } = require('vnftjs')
+
+const command = new Command()
+command.name = 'green'
+
+command.funct = async (bot, message, args) => {
+  const canvas = await loadCanvasByImage(args || message.author.avatarURL)
+  applyFilterToImageData(canvs, filter)
+
+  const attachment = new Discord.Attachment(canvas.toBuffer(), `user ${message.author.username}.png`)
+  message.channel.send(``, attachment)
+}
+
+function filter(cxd) {
+  let subPixelCount = Object.keys(cxd.data).length
+  for (let i = 0; i < subPixelCount; i += 4) {
+    cxd.data[i + 0] = 0
+    cxd.data[i + 2] = 0
+  }
+  return cxd
+}
+
+module.exports = command
