d4e2771070123faa706a589bc8a8aebb74044329
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-10-03 12:00:00 +0200

    added cyan

---
+++ anon/commands/canvas/filterino/cyan.js
@@ -0,0 +1,26 @@
+const loadCanvasByImage = require('./helperFunctions/loadCanvasByImage')
+const applyFilterToImageData = require('./helperFunctions/applyFilterToImageData')
+
+const Discord = require('discord.js')
+const { Command } = require('vnftjs')
+
+const command = new Command()
+command.name = 'cyan'
+
+command.funct = async (bot, message, args) => {
+  const canvas = await loadCanvasByImage(args || message.author.avatarURL)
+  applyFilterToImageData(canvs, filter)
+
+  const attachment = new Discord.Attachment(canvas.toBuffer(), `user ${message.author.username}.png`)
+  message.channel.send(``, attachment)
+}
+
+function filter(cxd) {
+  let subPixelCount = Object.keys(cxd.data).length
+  for (let i = 0; i < subPixelCount; i += 4) {
+    cxd.data[i + 1] = 0
+  }
+  return cxd
+}
+
+module.exports = command
