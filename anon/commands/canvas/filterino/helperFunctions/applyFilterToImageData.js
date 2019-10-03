07664ce44d04c12d4e3fd5fb086958d72510ee68
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-10-03 13:00:00 +0200

    prettier

--- anon/commands/canvas/filterino/helperFunctions/applyFilterToImageData.js
+++ anon/commands/canvas/filterino/helperFunctions/applyFilterToImageData.js
@@ -1,7 +1,7 @@
 function applyFilterToImageData(canvas, funct, ...args) {
   const ctx = canvas.getContext('2d')
   const cxd = ctx.getImageData(0, 0, canvas.width, canvas.height)
-  ctx.putImageData(funct(cxd,...args), 0, 0)
+  ctx.putImageData(funct(cxd, ...args), 0, 0)
 }
 
 module.exports = applyFilterToImageData
