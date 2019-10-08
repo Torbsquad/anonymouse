4b73f0b27b76476e6081ff0ed1acaf7e275b46c6
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-10-08 21:59:19 +0200

    allowing additional parameter

--- anon/commands/canvas/FilterCommand.js
+++ anon/commands/canvas/FilterCommand.js
@@ -3,15 +3,21 @@ const loadCanvasByImage = require('./helperFunctions/loadCanvasByImage')
 
 class FilterCommand extends Command {
   constructor(filter) {
+    this.bind(filter)
+  }
+
+  bind(filter) {
     super()
     this.funct = async (bot, message, args) => {
-      const canvas = await loadCanvasByImage(args || message.author.avatarURL)
-      applyFilterToImageData(canvas, filter)
+      const arrgs = args.split(' ')
+      const canvas = await loadCanvasByImage(arrgs[1] || message.author.avatarURL)
+      applyFilterToImageData(canvas, filter, arrgs[0])
 
       const attachment = new Discord.Attachment(canvas.toBuffer(), `FilterCommand-Image.png`)
       message.channel.send(``, attachment)
     }
   }
+  
 }
 
 module.exports = FilterCommand
