1850e67be2c42a6a712634eea143efe335b8fb46
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-10-09 11:19:38 +0200

    pointing to kuu/scripts

--- kuu/main.js
+++ kuu/main.js
@@ -2,6 +2,6 @@ const { CommandHandler } = require('vnftjs')
 const client = new CommandHandler()
 
 //client.loadCommands('./commands')
-client.loadScripts('./scripts')
+client.loadScripts('./kuu/scripts')
 
 client.login(process.env.plant)
