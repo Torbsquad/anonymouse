b6635981b61c9cf978603ac00688d135f7489d33
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-10-09 11:11:20 +0200

    removing commands

--- kuu/main.js
+++ kuu/main.js
@@ -1,7 +1,7 @@
 const { CommandHandler } = require('vnftjs')
 const client = new CommandHandler()
 
-client.loadCommands('./commands')
+//client.loadCommands('./commands')
 client.loadScripts('./scripts')
 
 client.login(process.env.plant)
