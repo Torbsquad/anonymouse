cb340b8ce2d183ce37d2c84939445371c888538c
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-10-09 11:10:16 +0200

    removing nuu from path

--- kuu/main.js
+++ kuu/main.js
@@ -1,7 +1,7 @@
 const { CommandHandler } = require('vnftjs')
 const client = new CommandHandler()
 
-client.loadCommands('./nuu/commands')
-client.loadScripts('./nuu/scripts')
+client.loadCommands('./commands')
+client.loadScripts('./scripts')
 
 client.login(process.env.plant)
