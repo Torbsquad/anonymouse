dd6ec6d716df9d889162678c0e07b76062e04752
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-10-11 22:05:24 +0200

    implemented FilterCommand

--- anon/commands/canvas/filters/edges.js
+++ anon/commands/canvas/filters/edges.js
@@ -1,21 +1,11 @@
-const loadCanvasByImage = require('../helperFunctions/loadCanvasByImage')
-const applyFilterToImageData = require('../helperFunctions/applyFilterToImageData')
 const getPixel = require('../helperFunctions/getPixel')
 const getIndex = require('../helperFunctions/getIndex')
 
 const Discord = require('discord.js')
-const { Command } = require('vnftjs')
+const FilterCommand = require('../FilterCommand')
 
-const command = new Command()
+const command = new FilterCommand(filter)
 command.name = 'edges'
-
-command.funct = async (bot, message, args) => {
-  const canvas = await loadCanvasByImage(args || message.author.avatarURL)
-  applyFilterToImageData(canvas, filter)
-
-  const attachment = new Discord.Attachment(canvas.toBuffer(), `user ${message.author.username}.png`)
-  message.channel.send(``, attachment)
-}
 
 function filter(cxd) {
   let subPixelCount = Object.keys(cxd.data).length
