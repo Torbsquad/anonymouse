839e8c0130885fed35144ad40b428a040088f817
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-10-15 19:13:15 +0200

    introducing FilterCommand

--- anon/commands/canvas/filters/colors/blue.js
+++ anon/commands/canvas/filters/colors/blue.js
@@ -1,19 +1,7 @@
-const loadCanvasByImage = require('../../helperFunctions/loadCanvasByImage')
-const applyFilterToImageData = require('../../helperFunctions/applyFilterToImageData')
+const FilterCommand = require('../FilterCommand')
 
-const Discord = require('discord.js')
-const { Command } = require('vnftjs')
-
-const command = new Command()
+const command = new FilterCommand(filter)
 command.name = 'blue'
-
-command.funct = async (bot, message, args) => {
-  const canvas = await loadCanvasByImage(args || message.author.avatarURL)
-  applyFilterToImageData(canvs, filter)
-
-  const attachment = new Discord.Attachment(canvas.toBuffer(), `user ${message.author.username}.png`)
-  message.channel.send(``, attachment)
-}
 
 function filter(cxd) {
   let subPixelCount = Object.keys(cxd.data).length
