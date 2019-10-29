bc1e0d4e03c43a6b0c41ac8ce9b92cc54de07bc4
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-10-29 18:33:43 +0100

    doublequotes to singlequotes

--- web/root/public/longtweet/main.js
+++ web/root/public/longtweet/main.js
@@ -1,31 +1,31 @@
-var input = document.getElementById("input");
-var outputText = document.getElementById("outputText");
-var outputImage = document.getElementById("outputImage");
-var textBlock = document.getElementById("textBlock");
-var imageBlock = document.getElementById("imageBlock");
+var input = document.getElementById('input')
+var outputText = document.getElementById('outputText')
+var outputImage = document.getElementById('outputImage')
+var textBlock = document.getElementById('textBlock')
+var imageBlock = document.getElementById('imageBlock')
 
 async function updateText() {
-  show(textBlock);
-  hide(imageBlock);
+  show(textBlock)
+  hide(imageBlock)
 
   outputText.innerHTML = input.value
     .replace(/\[o\]/g, '<span class="marked">')
-    .replace(/\[\/o\]/g, "</span>")
-    .replace(/\n/g, "</br>");
+    .replace(/\[\/o\]/g, '</span>')
+    .replace(/\n/g, '</br>')
 
-  var canvas = await html2canvas(outputText);
-  outputImage.innerHTML = "";
-  outputImage.appendChild(canvas);
+  var canvas = await html2canvas(outputText)
+  outputImage.innerHTML = ''
+  outputImage.appendChild(canvas)
 
-  hide(textBlock);
-  show(imageBlock);
+  hide(textBlock)
+  show(imageBlock)
 }
 
 function hide(el) {
-  el.style.display = "none";
+  el.style.display = 'none'
 }
 function show(el) {
-  el.style.display = "";
+  el.style.display = ''
 }
 
-updateText();
+updateText()
