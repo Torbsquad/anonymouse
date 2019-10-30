f4c844314820946fd0d38679a1d946d339aa8ecb
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-10-30 12:00:00 +0100

    more experiments with sinus

--- web/root/public/pinsel/main.js
+++ web/root/public/pinsel/main.js
@@ -1,10 +1,11 @@
 function main() {
   leinwand = new Leinwand('leinwand')
+  let size
 
   var l = new Line()
   l.color = 'green'
   leinwand.addLine(l)
-  let size = 0.1
+  size = .1
   for (let i = 0; i < 400; i += 2) {
     if (i < 50) {
       size += (10 - size) * 0.05
@@ -18,29 +19,29 @@ function main() {
   var l = new Line()
   l.color = 'red'
   leinwand.addLine(l)
-  for (let i = 0; i < 10; i += 0.05) {
-    let size = 10
-    if (i < 2) {
-      size = i * 5 + 0.1
+  size = .1
+  for (let i = 0; i < 400; i += 2) {
+    if (i < 50) {
+      size += (10 - size) * 0.05
     }
-    if (i > 8) {
-      size = (10 - i) * 5 + 0.1
+    if (i > 350) {
+      size += (0 - size) * 0.06
     }
-    l.addDot(50 + i * i * 4, 50 + i * 25, size)
+    l.addDot(50 + i, 50 + Math.sqrt(i) * 5 * Math.sin(i/2) + i, size)
   }
 
   var l = new Line()
   l.color = 'blue'
   leinwand.addLine(l)
-  for (let i = 0; i < 10; i += 0.05) {
-    let size = 10
-    if (i < 2) {
-      size = i * 5 + 0.1
+  size = .1
+  for (let i = 0; i < 400; i += 2) {
+    if (i < 50) {
+      size += (10 - size) * 0.05
     }
-    if (i > 8) {
-      size = (10 - i) * 5 + 0.1
+    if (i > 350) {
+      size += (0 - size) * 0.06
     }
-    l.addDot(50 + i * i * 4, 50 + i * 40, size)
+    l.addDot(50 + i, 50 + Math.sqrt(i) * 20, size*Math.sin(i/10)*1+size*1.5)
   }
 
   leinwand.draw()
