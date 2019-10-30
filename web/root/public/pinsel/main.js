a17cb72a588a6e768b42f9f7def2eb5c5768267b
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-10-30 12:00:00 +0100

    added an example stroke

--- web/root/public/pinsel/main.js
+++ web/root/public/pinsel/main.js
@@ -2,8 +2,15 @@ function main() {
   leinwand = new Leinwand('leinwand')
 
   var l = new Line()
-  for(let i = 0; i < 10; i+=.1){
-    l.addDot(100 + i*i*2, 100 + i*2, i*i+10)
+  for (let i = 0; i < 10; i += 0.05) {
+    let size = 10
+    if (i < 2) {
+      size = i * 5 + 0.1
+    }
+    if (i > 8) {
+      size = (10 - i) * 5 + 0.1
+    }
+    l.addDot(100 + i * i * 3, 100 + i * 20, size)
   }
   leinwand.addLine(l)
 
