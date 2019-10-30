93938420972ea1af68e495fdf96934834e48a4b3
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-10-30 12:00:00 +0100

    experimented with sin curves

--- web/root/public/pinsel/main.js
+++ web/root/public/pinsel/main.js
@@ -4,16 +4,15 @@ function main() {
   var l = new Line()
   l.color = 'green'
   leinwand.addLine(l)
-
-  for (let i = 0; i < 10; i += 0.05) {
-    let size = 10
-    if (i < 2) {
-      size = i * 5 + 0.1
+  let size = 0.1
+  for (let i = 0; i < 400; i += 2) {
+    if (i < 50) {
+      size += (10 - size) * 0.05
     }
-    if (i > 8) {
-      size = (10 - i) * 5 + 0.1
+    if (i > 350) {
+      size += (0 - size) * 0.06
     }
-    l.addDot(100 + i * i * 3, 100 + i * 10, size)
+    l.addDot(50 + i, 50 + Math.sqrt(i) * 5 + Math.sin(i / 10) * 10, size)
   }
 
   var l = new Line()
@@ -27,7 +26,7 @@ function main() {
     if (i > 8) {
       size = (10 - i) * 5 + 0.1
     }
-    l.addDot(100 + i * i * 3, 100 + i * 20, size)
+    l.addDot(50 + i * i * 4, 50 + i * 25, size)
   }
 
   var l = new Line()
@@ -41,7 +40,7 @@ function main() {
     if (i > 8) {
       size = (10 - i) * 5 + 0.1
     }
-    l.addDot(100 + i * i * 3, 100 + i * 35, size)
+    l.addDot(50 + i * i * 4, 50 + i * 40, size)
   }
 
   leinwand.draw()
