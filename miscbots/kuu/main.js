2434a3944376e37d2ee014a59338836cf1a33832
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-10-31 20:26:58 +0100

    pathupdate

--- miscbots/kuu/main.js
+++ miscbots/kuu/main.js
@@ -2,6 +2,6 @@ const { CommandHandler } = require('vnftjs')
 const client = new CommandHandler()
 
 //client.loadCommands('./commands')
-client.loadScripts('./miscbotskuu/scripts')
+client.loadScripts('./miscbots/kuu/scripts')
 
 client.login(process.env.plant)
