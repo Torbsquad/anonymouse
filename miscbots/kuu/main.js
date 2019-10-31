bde63d0af8a88b19404aef40f5eb0cb2321d721e
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-10-31 20:26:35 +0100

    fixing loadScripts-path

--- miscbots/kuu/main.js
+++ miscbots/kuu/main.js
@@ -2,6 +2,6 @@ const { CommandHandler } = require('vnftjs')
 const client = new CommandHandler()
 
 //client.loadCommands('./commands')
-client.loadScripts('./kuu/scripts')
+client.loadScripts('./miscbotskuu/scripts')
 
 client.login(process.env.plant)
