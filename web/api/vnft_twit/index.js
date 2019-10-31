c2bae871fbbda736a46d87dc3e7b8a2cdbbaa7a5
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-10-31 20:20:23 +0100

    preparing 2nd twit client

---
+++ web/api/vnft_twit/index.js
@@ -0,0 +1,16 @@
+/*
+
+const Twit = require('twit')
+
+var T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY2,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET2,
+  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY2,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET2,
+  timeout_ms: 60 * 1000,
+  strictSSL: true,
+})
+
+module.exports = T
+
+*/
