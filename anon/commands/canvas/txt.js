0d7f464d886b956f1f44d3fd6b018f5913c2ad8d
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-11-09 19:45:36 +0100

    added txt.js

---
+++ anon/commands/canvas/txt.js
@@ -0,0 +1,18 @@
+const Canvas = require('canvas')
+const Discord = require('discord.js')
+const { Command } = require('vnftjs')
+
+const command = new Command()
+command.name = 'text'
+
+command.funct = async (bot, message, args) => {
+  const canvas = Canvas.createCanvas(100, 100)
+  const ctx = canvas.getContext('2d')
+
+  ctx.fillText(args,10,10)
+
+  const attachment = new Discord.Attachment(canvas.toBuffer(), `a.png`)
+  message.channel.send(args, attachment)
+}
+
+module.exports = command
