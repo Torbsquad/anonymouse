d29f774e91226b38a26783695662a00de355d17f
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-11-16 20:49:43 +0100

    added orange

---
+++ anon/commands/canvas/filters/colors/orange.js
@@ -0,0 +1,14 @@
+const FilterCommand = require('../../FilterCommand')
+const command = new FilterCommand(filter)
+command.name = 'orange'
+
+function filter(cxd) {
+  let subPixelCount = Object.keys(cxd.data).length
+  for (let i = 0; i < subPixelCount; i += 4) {
+    cxd.data[i + 1] = Number(cxd.data[i + 1]) / 2
+    cxd.data[i + 2] = Number(cxd.data[i + 1]) / 2
+  }
+  return cxd
+}
+
+module.exports = command
