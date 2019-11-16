3691591caca9ed3d65e22a12dfb9a4d95a7881db
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-11-16 21:00:06 +0100

    removing blue of orange

--- anon/commands/canvas/filters/colors/orange.js
+++ anon/commands/canvas/filters/colors/orange.js
@@ -5,8 +5,9 @@ command.name = 'orange'
 function filter(cxd) {
   let subPixelCount = Object.keys(cxd.data).length
   for (let i = 0; i < subPixelCount; i += 4) {
+    cxd.data[i + 0] = Number(cxd.data[i + 0]) / 2
     cxd.data[i + 1] = Number(cxd.data[i + 1]) / 2
-    cxd.data[i + 2] = Number(cxd.data[i + 1]) / 2
+    cxd.data[i + 2] = 0
   }
   return cxd
 }
