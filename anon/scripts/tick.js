6f8b4c261885265def0b65a010c390a1bca1b363
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-11-19 14:41:39 +0100

    added bot.tick with time on it after Hi! has finished

---
+++ anon/scripts/tick.js
@@ -0,0 +1,15 @@
+const { Script } = require('vnftjs')
+const { sleep } = require('vnft-tools')
+
+const script = new Script()
+
+function tick(bot){
+  bot.user.setActivity(new Date().toString().split(" ")[4])
+}
+
+script.funct = async bot => {
+  await sleep(10000)
+  setInterval(bot=>tick(bot),5000)
+}
+
+module.exports = script
