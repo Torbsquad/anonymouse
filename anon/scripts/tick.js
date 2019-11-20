ac3e4594955bcb2b37859f354f7cb5968ae08d8a
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-11-20 23:35:05 +0100

    prettier

--- anon/scripts/tick.js
+++ anon/scripts/tick.js
@@ -3,13 +3,15 @@ const { sleep } = require('vnft-tools')
 
 const script = new Script()
 
-function tick(bot){
-  bot.user.setActivity(new Date().toString().split(" ")[4])
+function tick(bot) {
+  bot.user.setActivity(new Date().toString().split(' ')[4])
 }
 
 script.funct = async bot => {
   await sleep(10000)
-  setInterval(function(){tick(bot)},990)
+  setInterval(function() {
+    tick(bot)
+  }, 990)
 }
 
 module.exports = script
