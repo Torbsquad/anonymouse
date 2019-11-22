e0a1b2a2c4b65294322872af520942473bce906f
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-11-22 12:59:17 +0100

    adding time until 15:00 GMT+1; removing seconds in current timer

--- anon/scripts/tick.js
+++ anon/scripts/tick.js
@@ -4,14 +4,39 @@ const { sleep } = require('vnft-tools')
 const script = new Script()
 
 function tick(bot) {
-  bot.user.setActivity(new Date().toString().split(' ')[4])
+  let activity =  currentTime()+" - "+getTimeUntil(14)
+  bot.user.setActivity(activity)
+}
+
+function currentTime(){
+  let time = new Date().toString().split(" ")[4]
+  time = activity.split(":")
+  time.shift()
+  time = time.join(":")
+  return time
+}
+
+function doubleDigits(number){
+  number = (number).toString()
+  while(number.length < 2) number = "0" + number
+  return number
+}
+
+function getTimeUntil(hh,mm=0){
+  let d = new Date()
+  let e = new Date()
+  e.setHours(hh)
+  e.setMinutes(mm)
+  e.setSeconds(0)
+  let f = new Date(e-d)
+  return doubleDigits(f.getHours())+":"+doubleDigits(f.getMinutes())
 }
 
 script.funct = async bot => {
   await sleep(10000)
   setInterval(function() {
     tick(bot)
-  }, 990)
+  }, 30000)
 }
 
 module.exports = script
