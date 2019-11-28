a680105e964b4a6a26249a07ff88d9ac3ec209b6
floofyboi <47008528+floofyboi@users.noreply.github.com>
2019-11-28 23:55:11 +0100

    Update ffxspy.js

--- chocobot/scripts/ffxspy.js
+++ chocobot/scripts/ffxspy.js
@@ -5,20 +5,13 @@ const { get } = require('axios')
 const { RichEmbed } = require('discord.js')
 
 const TARGET_CHANNEL = '539802587239677963'
-const TARGET_CHARACTERS = [
-  20859306, // Bellini Lewo
-  20864548, // Na'il Lateef
-  21349090, // Diya Na'il
-  21853274, // Eava Wright
-  23945254, // Lily Rose (Sakura Lily)
-  6862438, //  Lena Lorien
+const TARGET_CHARACTERS = [ 
+  23945254, // Lily Rose (Sakura Lily)  
   25212840, // Aba Remillard
   3206978, //  Abaddon Seraph
   15530979, // Ava Aoyagi
-  9894204, //  Miyoki Malicera
-  22149004, // Nai Suru
-  23076248, // Atinuviel I'lave
-  12506676, // G'anta Kasper
+  9894204, //  Miyoki Malicera  
+  23076248, // Atinuviel I'lave  
 ]
 
 const TARGET_ATTRIBUTES = ['Name', 'Server', 'Race', 'Gender']
