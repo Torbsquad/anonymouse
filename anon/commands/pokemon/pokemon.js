13737d82db2590d15855674775508638cc5fe7a8
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-11-30 20:57:57 +0100

    added translation and image-url

--- anon/commands/pokemon/pokemon.js
+++ anon/commands/pokemon/pokemon.js
@@ -4,13 +4,20 @@ const axios = require("axios")
 const command = new Command()
 command.name = 'pk'
 command.funct = async (bot, message, args) => {
-  let url = `https://pokeapi.co/api/v2/pokemon/${args}`
+  let pokewiki_url = `https://www.pokewiki.de/${args}`
+  let pokewiki = await axios.get(pokewiki_url)
+
+  let name = pokewiki.data.match(/<b>(.*?)<\/b>/g)[2].match(/<b>(.*?)<\/b>/)[1]
+  
+  
+  let url = `https://pokeapi.co/api/v2/pokemon/${name}`
   let request = await axios.get(url)
   
   let p = request.data
   var d = {}
   var f = p.stats.map(e=>{d[e.stat.name] = e.base_stat})
   d.name = p.name
+  d.image = p.data.sprites.front_default
   d.abilities = p.abilities.sort(e=>e.is_hidden).map(e=>`${e.ability.name}${e.is_hidden?' (hidden)':''}`).join(", ")
   
   message.reply(JSON.stringify(d,null,2))
