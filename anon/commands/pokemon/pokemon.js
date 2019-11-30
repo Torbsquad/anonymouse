e31a4c49aec1c3367842f7237cd9b16677aa82e7
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-11-30 21:11:59 +0100

    made name lowercase to fix api request

--- anon/commands/pokemon/pokemon.js
+++ anon/commands/pokemon/pokemon.js
@@ -7,8 +7,7 @@ command.funct = async (bot, message, args) => {
   let pokewiki_url = `https://www.pokewiki.de/${args}`
   let pokewiki = await axios.get(pokewiki_url)
 
-  let name = pokewiki.data.match(/<b>(.*?)<\/b>/g)[2].match(/<b>(.*?)<\/b>/)[1]
-  
+  let name = pokewiki.data.match(/<b>(.*?)<\/b>/g)[2].match(/<b>(.*?)<\/b>/)[1].toLowerCase()
   
   let url = `https://pokeapi.co/api/v2/pokemon/${name}`
   let request = await axios.get(url)
@@ -17,7 +16,7 @@ command.funct = async (bot, message, args) => {
   var d = {}
   var f = p.stats.map(e=>{d[e.stat.name] = e.base_stat})
   d.name = p.name
-  d.image = p.data.sprites.front_default
+  d.image = p.sprites.front_default
   d.abilities = p.abilities.sort(e=>e.is_hidden).map(e=>`${e.ability.name}${e.is_hidden?' (hidden)':''}`).join(", ")
   
   message.reply(JSON.stringify(d,null,2))
