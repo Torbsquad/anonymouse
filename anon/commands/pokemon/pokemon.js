b8f8728e402b3f0b55b56ceb81b4f3c62b372574
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-11-30 20:21:47 +0100

    added abilities and name

--- anon/commands/pokemon/pokemon.js
+++ anon/commands/pokemon/pokemon.js
@@ -7,14 +7,13 @@ command.funct = async (bot, message, args) => {
   let url = `https://pokeapi.co/api/v2/pokemon/${args}`
   let request = await axios.get(url)
   
+  let p = request.data
   var d = {}
-  var f = request.data.stats.map(e=>[e.stat.name,e.base_stat])
-
-  for(var o of f){
-      d[o[0]] = o[1]
-  }
+  var f = p.stats.map(e=>{d[e.stat.name] = e.base_stat})
+  d.name = p.name
+  d.abilities = p.abilities.sort(e=>e.is_hidden).map(e=>`${e.ability.name}${e.is_hidden?' (hidden)':''}`).join(", ")
   
-  message.reply(JSON.stringify(d))
+  message.reply(JSON.stringify(d,null,2))
 }
 
 module.exports = command
