43d96a6e08f9f0b265075187d7f08d6fec974244
green boi <46520916+Ultrigeon@users.noreply.github.com>
2019-12-02 19:41:16 +0100

    init v2

---
+++ anon/commands/pokemon/v2.js
@@ -0,0 +1,21 @@
+const { Command } = require('vnftjs')
+const Discord = require("discord.js")
+const axios = require("axios")
+ 
+const command = new Command()
+command.name = 'pp'
+command.funct = async (bot, message, args) => {
+  try{
+    let pokewiki_url = `https://www.pokewiki.de/${args}`
+    let pokewiki = await axios.get(pokewiki_url)
+
+    let name = pokewiki.data.match(/title=\"Englisch\">en<\/span><span.*?>(.*?)<\/span><\/div>/)[1].toLowerCase()
+    message.reply(args)
+    message.reply(name)
+  }
+  catch(err){
+    message.reply(err.message)
+  }
+}
+
+module.exports = command
