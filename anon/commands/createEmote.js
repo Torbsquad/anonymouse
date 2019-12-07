866d3e9510c73fdf73f4eea60cd1b8dd88732054
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-12-07 20:53:31 +0100

    prettier

--- anon/commands/createEmote.js
+++ anon/commands/createEmote.js
@@ -3,8 +3,8 @@ const { Command } = require('vnftjs')
 const command = new Command()
 command.name = 'createEmote'
 command.funct = async (bot, message, args) => {
-  try{
-    let a = args.split(" ")
+  try {
+    let a = args.split(' ')
     message.guild.createEmoji(a[1], a[0])
   } catch (err) {
     message.reply(err.message)
