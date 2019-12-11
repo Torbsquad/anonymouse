e2da4df327ab0deba5211584d3e30351e788730e
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-12-11 12:00:00 +0100

    removing ping

--- chocobot/commands/ping.js
+++
@@ -1,9 +0,0 @@
-const { Command } = require('vnftjs')
-
-const command = new Command()
-command.name = 'ping'
-command.funct = (bot, message, args) => {
-  message.reply('pong')
-}
-
-module.exports = command
