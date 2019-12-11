53af43e094e74f717891ec7299d6ab95b38cb6af
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-12-11 12:00:00 +0100

    removing left command

--- anon/commands/chat/leave.js
+++
@@ -1,9 +0,0 @@
-const { Command } = require('vnftjs')
-
-const command = new Command()
-command.name = 'leave'
-command.funct = function(bot, message, args) {
-  message.channel.send(`${message.author.username} left.`)
-}
-
-module.exports = command
