ffbb108c77f9765a36080e946d868babcc074dca
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-12-14 17:31:51 +0100

    on dev-mode, load only pk

--- web/api/index.js
+++ web/api/index.js
@@ -14,10 +14,14 @@ app.get('/', (req, res) => {
   res.json({ status: 'OK!' })
 })
 
-siteHandler.loadFolder(path.join(__dirname, 'twit'))
-siteHandler.loadFolder(path.join(__dirname, 'emoji'))
-siteHandler.loadFolder(path.join(__dirname, 'socket'))
-siteHandler.loadFolder(path.join(__dirname, 'nazrin'))
-siteHandler.loadFolder(path.join(__dirname, 'pk'))
+if (process.env.NODE_ENV == 'dev') {
+  siteHandler.loadFolder(path.join(__dirname, 'pk'))
+} else {
+  siteHandler.loadFolder(path.join(__dirname, 'twit'))
+  siteHandler.loadFolder(path.join(__dirname, 'emoji'))
+  siteHandler.loadFolder(path.join(__dirname, 'socket'))
+  siteHandler.loadFolder(path.join(__dirname, 'nazrin'))
+  siteHandler.loadFolder(path.join(__dirname, 'pk'))
+}
 
 module.exports = app
