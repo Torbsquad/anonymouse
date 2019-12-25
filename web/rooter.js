6ae9ae1e943626d55f276fef75f199f4fad086b4
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-12-25 20:03:13 +0100

    deleted unused folder

--- web/rooter.js
+++ web/rooter.js
@@ -20,5 +20,4 @@ if (process.env.NODE_ENV == 'dev') {
   app.use(vhost('socket.vnft.cc', require('./rpg')))
   app.use(vhost('vnft.cc', require('./root')))
   app.use(vhost('api.vnft.cc', require('./api')))
-  // app.use(vhost('anon-spirit.herokuapp.com', require('./core')))
 }
