7eda70435510fa4246d2a48646ea6bac39836013
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-12-25 20:04:14 +0100

    removed rpg.

--- web/rpg/public/classes/block.js
+++
@@ -1,5 +0,0 @@
-class Block extends GameObject {
-  constructor() {
-    super()
-  }
-}
--- web/rpg/public/classes/tileset.js
+++
@@ -1,8 +0,0 @@
-class Tileset {
-  constructor(path) {
-    this.image = new Image()
-    this.loaded = false
-    this.image.onload = () => (this.loaded = true)
-    this.image.src = path
-  }
-}
