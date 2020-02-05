b340049772ca4acf2596bbb29eb5c2a343b0b137
green boi <46520916+Ultrigeon@users.noreply.github.com>
2020-02-05 08:01:33 +0100

    Create insertUsers.js

---
+++ web/api/torb/insertUsers.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+const pg = require('../../../db')
+
+const { Site } = require('vnft-tools')
+const site = new Site('/torb/insertUsers')
+
+site.get = async (req, res) => {
+  try{
+    let users = axios.get('https://api.vnft.cc/torb/getUsers')
+    let insertQuery = `
+      insert into torbstatus(user_id, username)
+      values ${users.map(user => "(" + user.user_id + ", " + user.username + ")").join(", ")}
+    `
+    await pg.query(insertQuery)
+    res.json(insertQuery)
+  }
+  catch(err){
+    res.json(err.message)
+  }
+}
+
+module.exports = site
