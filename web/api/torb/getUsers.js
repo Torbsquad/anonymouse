2accee97698c9c3dea1c815cca43ac7f6c9f881e
green boi <46520916+Ultrigeon@users.noreply.github.com>
2020-02-05 07:45:11 +0100

    added try-catch-block

--- web/api/torb/getUsers.js
+++ web/api/torb/getUsers.js
@@ -5,14 +5,19 @@ const { Site } = require('vnft-tools')
 const site = new Site('/torb/getUsers')
 
 site.get = async (req, res) => {
-  var torb = nazrin.guilds.find(guild => guild.id == "209673980200615939")
-  var members = torb.members
-  var memberStatus = members.map(member => {
-    var username = member.user.username
-    var status = member.user.presence.status
-    return {username, status}
-  })
-  res.json(memberStatus)
+  try{
+    var torb = nazrin.guilds.find(guild => guild.id == "209673980200615939")
+    var members = torb.members
+    var memberStatus = members.map(member => {
+      var username = member.user.username
+      var status = member.user.presence.status
+      return {username, status}
+    })
+    res.json(memberStatus)
+  }
+  catch(err){
+    res.json(err.message)
+  }
 }
 
 module.exports = site
