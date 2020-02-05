6e1667b6254363d007b0d702bd6a15f1189d9025
green boi <46520916+Ultrigeon@users.noreply.github.com>
2020-02-05 07:39:30 +0100

    changed format of member-object

--- web/api/torb/getUsers.js
+++ web/api/torb/getUsers.js
@@ -7,7 +7,12 @@ const site = new Site('/torb/getUsers')
 site.get = async (req, res) => {
   var torb = nazrin.guilds.find(guild => guild.id == "209673980200615939")
   var members = torb.members
-  var memberStatus = members.map(member => [member.user.username, member.user.presence.status])
+  var memberStatus = members.map(member => {
+    return {
+      username: member.user.username, 
+      status: member.user.presence.status
+    }
+  )
   res.json(memberStatus)
 }
 
