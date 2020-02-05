4802c578c21b5d0e35215824b217a199107eac3b
green boi <46520916+Ultrigeon@users.noreply.github.com>
2020-02-05 08:07:43 +0100

    added await before request

--- web/api/torb/insertUsers.js
+++ web/api/torb/insertUsers.js
@@ -6,10 +6,10 @@ const site = new Site('/torb/insertUsers')
 
 site.get = async (req, res) => {
   try{
-    let users = axios.get('https://api.vnft.cc/torb/getUsers')
+    let users = await axios.get('https://api.vnft.cc/torb/getUsers')
     let insertQuery = `
       insert into torbstatus(user_id, username)
-      values ${users.map(user => "(" + user.user_id + ", " + user.username + ")").join(", ")}
+      values ${users.data.map(user => "(" + user.user_id + ", " + user.username + ")").join(", ")}
     `
     //await pg.query(insertQuery)
     res.json(insertQuery)
