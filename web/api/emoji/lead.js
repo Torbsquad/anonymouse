49625bf65a4baf1815240a7a036a4196f1cda033
VonFriedricht <VonFriedricht@pm.me>
2021-02-08 00:10:42 +0100

    removing options

--- web/api/emoji/lead.js
+++ web/api/emoji/lead.js
@@ -4,11 +4,8 @@ const { Site } = require('vnft-tools')
 const all = new Site('/emoji/lead/:page')
 
 all.get = async (req, res) => {
-  let query = `SELECT * FROM EMOJIS2 ORDER BY POINTS DESC OFFSET $(offset) LIMIT $(limit)`
-  let options = {
-    offset: Number(req.params.page) * 100,
-    limit: 100,
-  }
+  let query = `SELECT * FROM EMOJIS2 ORDER BY POINTS DESC OFFSET $1 LIMIT $2`
+  let values = [Number(req.params.page) * 100, 100]
   let re = await pg.query(query, options)
   console.log(re)
   res.json(re)
