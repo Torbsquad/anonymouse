2a8c49f2b748d3eed582f50b34464fb85dad9fa4
VonFriedricht <VonFriedricht@pm.me>
2021-03-09 02:29:36 +0100

    implemented frontend

---
+++ web/public/emoji/script.js
@@ -0,0 +1,28 @@
+var currentPage = 0
+loadset(currentPage++)
+
+function addEmote(emote){
+    var content = document.getElementById("content")
+    var emoteHolder = document.createElement("div")
+    emoteHolder.className = "emoteholder"
+    var icon = document.createElement("img")
+    icon.className = "icon"
+    icon.src = `https://cdn.discordapp.com/emojis/${emote.id}.${emote.animated?"gif":"png"}`
+    emoteHolder.appendChild(icon)
+    content.appendChild(emoteHolder)
+}
+
+function loadset(page){
+    axios.get(`https://vnft.cc/api/emoji/list/${page}`)
+        .then(e=>{
+            console.log(e.data.forEach(addEmote))
+        })
+}
+
+function checkend(){
+    if(window.scrollMaxY-400 < window.scrollY){       
+        loadset(currentPage++)
+    }
+}
+
+setInterval(checkend, 500)
