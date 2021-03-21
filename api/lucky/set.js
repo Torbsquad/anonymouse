5501bd766c3205fe4cb2e7f1ca684295634ae156
VonFriedricht <Vonfriedricht@pm.me>
2021-03-21 02:13:26 +0100

    replacing %47 with /

--- api/lucky/set.js
+++ api/lucky/set.js
@@ -2,6 +2,8 @@ module.exports = async function(input){
     try{
         const client = require("../../connect.js")
         
+        input = input.replace(/%47/g,"/")
+
         const res = await client.query(`
             UPDATE LUCKY SET VALUE = $1
         `, [input])
